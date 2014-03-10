04ba96b09b91b51ab34994d6c0cd4453fdfc2754
shadowmage45 <shadowmage45@hotmail.com>
2014-03-10 09:36:27 -0600

    add bounding-box rendering for scanner

--- src/main/java/net/shadowmage/ancientwarfare/structure/proxy/ClientProxyStructures.java
+++ src/main/java/net/shadowmage/ancientwarfare/structure/proxy/ClientProxyStructures.java
@@ -1,7 +1,9 @@
 package net.shadowmage.ancientwarfare.structure.proxy;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.shadowmage.ancientwarfare.core.network.NetworkHandler;
 import net.shadowmage.ancientwarfare.core.proxy.ClientProxyBase;
+import net.shadowmage.ancientwarfare.structure.event.StructureBoundingBoxRenderer;
 import net.shadowmage.ancientwarfare.structure.gui.GuiStructureScanner;
 
 public class ClientProxyStructures extends ClientProxyBase
@@ -11,5 +13,6 @@ public class ClientProxyStructures extends ClientProxyBase
 public void registerClient()
   {
   NetworkHandler.INSTANCE.registerGui(NetworkHandler.GUI_SCANNER, GuiStructureScanner.class);
+  MinecraftForge.EVENT_BUS.register(StructureBoundingBoxRenderer.instance());
   }
 }
