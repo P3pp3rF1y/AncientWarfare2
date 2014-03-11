d18689383b53c2f8d67f83eee4f9e702ff9215a7
shadowmage45 <shadowmage45@hotmail.com>
2014-03-11 14:22:05 -0600

    add structure-sort ability to builder-gui. still need to add container-side code to get initial structure name, and set name on gui-close

--- src/main/java/net/shadowmage/ancientwarfare/structure/gui/GuiStructureSelection.java
+++ src/main/java/net/shadowmage/ancientwarfare/structure/gui/GuiStructureSelection.java
@@ -1,13 +1,18 @@
 package net.shadowmage.ancientwarfare.structure.gui;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 import net.minecraft.util.StatCollector;
 import net.shadowmage.ancientwarfare.core.container.ContainerBase;
 import net.shadowmage.ancientwarfare.core.gui.GuiContainerBase;
+import net.shadowmage.ancientwarfare.core.gui.Listener;
 import net.shadowmage.ancientwarfare.core.gui.elements.Button;
 import net.shadowmage.ancientwarfare.core.gui.elements.CompositeScrolled;
+import net.shadowmage.ancientwarfare.core.gui.elements.GuiElement;
 import net.shadowmage.ancientwarfare.core.gui.elements.Label;
 import net.shadowmage.ancientwarfare.core.gui.elements.Text;
 import net.shadowmage.ancientwarfare.structure.template.StructureTemplateClient;
@@ -23,15 +28,34 @@ Label selection;
 
 HashMap<Label, StructureTemplateClient> templateMap = new HashMap<Label, StructureTemplateClient>();
 
+ComparatorStructureTemplateClient sorter;
+
 public GuiStructureSelection(ContainerBase par1Container)
   {
   super(par1Container, 256, 240, defaultBackground);
+  sorter = new ComparatorStructureTemplateClient();
+  sorter.setFilterText("");
+  /**
+   * TODO set initial selection name from player.currentItem
+   */
   }
 
 @Override
 public void initElements()
   {
-  addGuiElement(new Button(256-55-8, 8, 55, 12, StatCollector.translateToLocal("guistrings.done")));
+  
+  addGuiElement(new Button(256-55-8, 8, 55, 12, StatCollector.translateToLocal("guistrings.done"))
+    {
+    @Override
+    protected void onPressed()
+      {
+      
+      /**
+       * TODO close GUI
+       * send packet to server-side container to set selected name onto item
+       */
+      }
+    });  
   
   selection = new Label(8,8, "");
   addGuiElement(selection);
@@ -39,7 +63,15 @@ public void initElements()
   selectionArea = new CompositeScrolled(0, 40, 256, 200);
   addGuiElement(selectionArea);
   
-  filterInput = new Text(8, 24, 240-16, "", this);
+  filterInput = new Text(8, 24, 240-16, "", this)
+    {
+    @Override
+    protected void handleKeyInput(int keyCode, char ch)
+      {      
+      super.handleKeyInput(keyCode, ch);
+      refreshGui();
+      }
+    };
   addGuiElement(filterInput);  
   }
 
@@ -50,26 +82,42 @@ public void setupElements()
   templateMap.clear();
   setSelectionName((currentSelection==null? StatCollector.translateToLocal("guistrings.none") : currentSelection.name));  
   
-  Collection<StructureTemplateClient> templates = StructureTemplateManager.instance().getClientStructures();
+  Collection<StructureTemplateClient> templatesC = StructureTemplateManager.instance().getClientStructures();
+  List<StructureTemplateClient> templates = new ArrayList<StructureTemplateClient>();
+  templates.addAll(templatesC);
+  sorter.setFilterText(filterInput.getText());  
+  Collections.sort(templates, sorter);
   
-  /**
-   * TODO sort templates by filterInput
-   */
-  
-  /**
-   * TODO add listener to labels for selection
-   */
+  Listener listener = new Listener(Listener.MOUSE_UP)
+    {
+    @Override
+    public boolean onEvent(GuiElement widget, ActivationEvent evt)
+      {
+      if(widget.isMouseOverElement(evt.mx, evt.my))
+        {   
+        setSelection(templateMap.get((Label)widget));
+        }
+      return true;
+      }
+    };
   
   Label label = null;
   int totalHeight = 3;
   for(StructureTemplateClient template : templates)
     {
     label = new Label(8, totalHeight, template.name);
+    label.addNewListener(listener);
     selectionArea.addGuiElement(label);
     templateMap.put(label, template);
     totalHeight+=12;
     }
   selectionArea.setAreaSize(totalHeight);
+  }
+
+private void setSelection(StructureTemplateClient template)
+  {
+  this.currentSelection = template;
+  this.setSelectionName(template==null? StatCollector.translateToLocal("guistrings.none") : template.name);
   }
 
 public void setSelectionName(String name)
