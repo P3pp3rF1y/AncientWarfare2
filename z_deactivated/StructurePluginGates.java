ef260589eee6650c3f53b66831bcbf90d035d849
shadowmage45 <shadowmage45@hotmail.com>
2014-03-24 13:00:43 -0600

    add gate handling plugin / rule from 1.6 version -- will need to adapt slightly for 1.7.

---
+++ z_deactivated/StructurePluginGates.java
@@ -0,0 +1,54 @@
+/**
+   Copyright 2012-2013 John Cummens (aka Shadowmage, Shadowmage4513)
+   This software is distributed under the terms of the GNU General Public License.
+   Please see COPYING for precise license information.
+
+   This file is part of Ancient Warfare.
+
+   Ancient Warfare is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Ancient Warfare is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with Ancient Warfare.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package shadowmage.ancient_structures.common.template.plugin.default_plugins;
+
+import shadowmage.ancient_structures.AWStructures;
+import shadowmage.ancient_structures.common.template.plugin.StructureContentPlugin;
+import shadowmage.ancient_structures.common.template.plugin.StructurePluginManager;
+import shadowmage.ancient_structures.common.template.plugin.default_plugins.entity_rules.TemplateRuleGates;
+import shadowmage.ancient_warfare.common.gates.EntityGate;
+
+public class StructurePluginGates extends StructureContentPlugin
+{
+
+public StructurePluginGates()
+  {
+ 
+  }
+
+@Override
+public void addHandledBlocks(StructurePluginManager manager)
+  {
+
+  }
+
+@Override
+public void addHandledEntities(StructurePluginManager manager)
+  {
+  manager.registerEntityHandler("awGates", EntityGate.class, TemplateRuleGates.class);
+  }
+
+public static void load()
+  {
+  AWStructures.instance.pluginManager.addPlugin(new StructurePluginGates());
+  }
+
+}
