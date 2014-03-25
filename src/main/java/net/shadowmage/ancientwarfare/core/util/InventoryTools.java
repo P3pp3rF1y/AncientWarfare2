faf8799119990ee3bfeddd4e7b9d7d66f2f9d0a9
shadowmage45 <shadowmage45@hotmail.com>
2014-03-25 11:30:27 -0600

    add basic inventory manipulation code/util class

---
+++ src/main/java/net/shadowmage/ancientwarfare/core/util/InventoryTools.java
@@ -0,0 +1,218 @@
+package net.shadowmage.ancientwarfare.core.util;
+
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.item.ItemStack;
+
+public class InventoryTools
+{
+
+/**
+ * Attempt to merge stack into inventory via the given side, or general all-sides merge if side <0<br>
+ * Resorts to default general merge if inventory is not a sided inventory.<br>
+ * Double-pass merging.  First pass attempts to merge with partial stacks.  Second pass will place
+ * into empty slots if available.
+ * @param inventory the inventory to merge into, must not be null
+ * @param stack the stack to merge, must not be null
+ * @param side or <0 for none
+ * @return any remaining un-merged item, or null if completely merged
+ */
+public static ItemStack mergeItemStack(IInventory inventory, ItemStack stack, int side)
+  {
+  if(side>0 && inventory instanceof ISidedInventory)
+    {
+    int[] slotIndices = ((ISidedInventory)inventory).getAccessibleSlotsFromSide(side);
+    if(slotIndices==null){return null;}
+    int index;
+    int toMove;
+    ItemStack slotStack;
+    for(int i = 0; i <slotIndices.length; i++)
+      {
+      toMove = stack.stackSize;
+      index = slotIndices[i];
+      slotStack = inventory.getStackInSlot(index);
+      if(doItemStacksMatch(stack, slotStack))
+        {
+        if(toMove > slotStack.getMaxStackSize() - slotStack.stackSize)
+          {
+          toMove = slotStack.getMaxStackSize() - slotStack.stackSize;
+          }
+        stack.stackSize-=toMove;
+        slotStack.stackSize+=toMove;
+        inventory.setInventorySlotContents(index, slotStack);
+        inventory.markDirty();
+        }      
+      if(stack.stackSize<=0)//merged stack fully;
+        {
+        break;
+        }
+      }
+    if(stack.stackSize>0)
+      {
+      for(int i = 0; i <slotIndices.length; i++)
+        {        
+        index = slotIndices[i];
+        slotStack = inventory.getStackInSlot(index);
+        if(slotStack==null)
+          {
+          inventory.setInventorySlotContents(index, stack);
+          inventory.markDirty();
+          return null;//successful merge
+          }
+        }
+      }
+    else
+      {
+      return null;//successful merge
+      }
+    }
+  else
+    {
+    int index;
+    int toMove;
+    ItemStack slotStack;
+    for(int i = 0; i <inventory.getSizeInventory(); i++)
+      {
+      toMove = stack.stackSize;
+      index = i;
+      slotStack = inventory.getStackInSlot(index);
+      if(doItemStacksMatch(stack, slotStack))
+        {
+        if(toMove > slotStack.getMaxStackSize() - slotStack.stackSize)
+          {
+          toMove = slotStack.getMaxStackSize() - slotStack.stackSize;
+          }
+        stack.stackSize-=toMove;
+        slotStack.stackSize+=toMove;
+        inventory.setInventorySlotContents(index, slotStack);
+        inventory.markDirty();
+        }      
+      if(stack.stackSize<=0)//merged stack fully;
+        {
+        break;
+        }
+      }
+    if(stack.stackSize>0)
+      {
+      for(int i = 0; i <inventory.getSizeInventory(); i++)
+        {        
+        index = i;
+        slotStack = inventory.getStackInSlot(index);
+        if(slotStack==null)
+          {
+          inventory.setInventorySlotContents(index, stack);
+          inventory.markDirty();
+          return null;//successful merge
+          }
+        }
+      }
+    else
+      {
+      return null;//successful merge
+      }
+    }
+  return stack;//partial or unsuccessful merge
+  }
+
+/**
+ * Attempts to remove filter * quantity from inventory.  Returns removed item in return stack, or null if
+ * no items were removed.
+ * @param inventory
+ * @param side
+ * @param toRemove
+ * @return the removed item.
+ */
+public static ItemStack removeItems(IInventory inventory, int side, ItemStack filter, int quantity)
+  {  
+  ItemStack returnStack = null;
+  if(side>0 && inventory instanceof ISidedInventory)
+    {
+    int[] slotIndices = ((ISidedInventory)inventory).getAccessibleSlotsFromSide(side);
+    if(slotIndices==null){return null;}
+    int index;
+    int toMove;
+    ItemStack slotStack;
+    for(int i = 0; i <slotIndices.length; i++)
+      {  
+      
+      index = slotIndices[i];
+      slotStack = inventory.getStackInSlot(index);
+      if(!doItemStacksMatch(slotStack, filter)){continue;}
+      if(returnStack==null)
+        {
+        returnStack = new ItemStack(filter.getItem());
+        returnStack.stackSize = 0;
+        }
+      toMove = slotStack.stackSize;
+      if(toMove>quantity){toMove = quantity;}
+      if(toMove + returnStack.stackSize> returnStack.getMaxStackSize()){toMove = returnStack.getMaxStackSize() - returnStack.stackSize;}
+      
+      returnStack.stackSize+=toMove;
+      slotStack.stackSize-=toMove;
+      quantity-=toMove;
+      if(slotStack.stackSize<=0)
+        {
+        inventory.setInventorySlotContents(index, null);        
+        }
+      inventory.markDirty();
+      if(quantity<=0)
+        {
+        break;
+        }
+      }
+    }
+  else
+    {
+    int index;
+    int toMove;
+    ItemStack slotStack;
+    for(int i = 0; i <inventory.getSizeInventory(); i++)
+      {        
+      index = i;
+      slotStack = inventory.getStackInSlot(index);
+      if(!doItemStacksMatch(slotStack, filter)){continue;}
+      if(returnStack==null)
+        {
+        returnStack = new ItemStack(filter.getItem());
+        returnStack.stackSize = 0;
+        }
+      toMove = slotStack.stackSize;
+      if(toMove>quantity){toMove = quantity;}
+      if(toMove + returnStack.stackSize> returnStack.getMaxStackSize()){toMove = returnStack.getMaxStackSize() - returnStack.stackSize;}
+      
+      returnStack.stackSize+=toMove;
+      slotStack.stackSize-=toMove;
+      quantity-=toMove;
+      if(slotStack.stackSize<=0)
+        {
+        inventory.setInventorySlotContents(index, null);        
+        }
+      inventory.markDirty();
+      if(quantity<=0)
+        {
+        break;
+        }
+      }
+    }  
+  return returnStack;
+  }
+
+/**
+ * validates that stacks are the same item / damage / tag, ignores quantity
+ * @param stack1
+ * @param stack2
+ * @return
+ */
+public static boolean doItemStacksMatch(ItemStack stack1, ItemStack stack2)
+  {
+  if(stack1==null){return stack2==null;}
+  if(stack2==null){return stack1==null;}
+  if(stack1.getItem()==stack2.getItem() && stack1.getItemDamage()==stack2.getItemDamage() && ItemStack.areItemStackTagsEqual(stack1, stack2))
+    {
+    return true;
+    }
+  return false;
+  }
+
+
+}
