1c24a413d03af2bcffab1e275e677e57b7e0ba91
shadowmage45 <shadowmage45@hotmail.com>
2014-04-16 14:51:09 -0600

    add research-book display icon to engineering table

--- src/main/java/net/shadowmage/ancientwarfare/core/gui/GuiEngineeringStation.java
+++ src/main/java/net/shadowmage/ancientwarfare/core/gui/GuiEngineeringStation.java
@@ -1,6 +1,9 @@
 package net.shadowmage.ancientwarfare.core.gui;
 
+import net.minecraft.item.ItemStack;
+import net.shadowmage.ancientwarfare.core.api.AWItems;
 import net.shadowmage.ancientwarfare.core.container.ContainerBase;
+import net.shadowmage.ancientwarfare.core.gui.elements.ItemSlot;
 
 public class GuiEngineeringStation extends GuiContainerBase
 {
@@ -13,7 +16,9 @@ public GuiEngineeringStation(ContainerBase par1Container)
 @Override
 public void initElements()
   {
-
+  ItemSlot bookSlotIcon = new ItemSlot(7, 7, new ItemStack(AWItems.researchBook), this);
+  bookSlotIcon.setRenderTooltip(false).setHighlightOnMouseOver(false).setRenderSlotBackground(false);
+  addGuiElement(bookSlotIcon);
   }
 
 @Override
