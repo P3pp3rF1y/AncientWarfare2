f14d50ab6bf1df190d6aa8939e49ecf6460a1c76
shadowmage45 <shadowmage45@hotmail.com>
2014-04-16 13:26:22 -0600

    add more control to setup of fake-item-slot gui widget

--- src/main/java/net/shadowmage/ancientwarfare/core/gui/elements/ItemSlot.java
+++ src/main/java/net/shadowmage/ancientwarfare/core/gui/elements/ItemSlot.java
@@ -24,6 +24,9 @@ public class ItemSlot extends GuiElement
 protected static RenderItem itemRender = new RenderItem();
 ItemStack item;
 protected ITooltipRenderer render;
+protected boolean highlightOnMouseOver = true;
+protected boolean renderTooltip = true;
+protected boolean renderSlotBackground = true;
 
 public ItemSlot(int topLeftX, int topLeftY, ItemStack item, ITooltipRenderer render)
   {
@@ -37,14 +40,35 @@ public void setItem(ItemStack item)
   this.item = item;
   }
 
+public ItemSlot setRenderTooltip(boolean val)
+  {
+  this.renderTooltip = val;
+  return this;
+  }
+
+public ItemSlot setHighlightOnMouseOver(boolean val)
+  {
+  this.highlightOnMouseOver = val;
+  return this;
+  }
+
+public ItemSlot setRenderSlotBackground(boolean val)
+  {
+  this.renderSlotBackground = val;
+  return this;
+  }
+
 @Override
 public void render(int mouseX, int mouseY, float partialTick)
   {
   if(visible && item!=null)
     {
     Minecraft mc = Minecraft.getMinecraft();    
-    mc.renderEngine.bindTexture(widgetTexture1);
-    RenderTools.renderQuarteredTexture(256, 256, 152, 120, 18, 18, renderX, renderY, width, height);
+    if(renderSlotBackground)
+      {
+      mc.renderEngine.bindTexture(widgetTexture1);
+      RenderTools.renderQuarteredTexture(256, 256, 152, 120, 18, 18, renderX, renderY, width, height);      
+      }
 
     GL11.glDisable(GL11.GL_DEPTH_TEST);
     if(this.item!=null && this.item.getItem()!=null)
@@ -56,31 +80,37 @@ public void render(int mouseX, int mouseY, float partialTick)
       
       GL11.glEnable(GL11.GL_LIGHTING);
       itemRender.renderItemAndEffectIntoGUI(font, mc.getTextureManager(), item, renderX+1, renderY+1);
-      itemRender.renderItemOverlayIntoGUI(font, mc.getTextureManager(), item, renderX+1, renderY+1, String.valueOf(item.stackSize));
+      if(renderTooltip)
+        {
+        itemRender.renderItemOverlayIntoGUI(font, mc.getTextureManager(), item, renderX+1, renderY+1, String.valueOf(item.stackSize));        
+        }
       GL11.glDisable(GL11.GL_LIGHTING);
       }    
     
     if(isMouseOverElement(mouseX, mouseY))
       {
-      /**
-       *  TODO -- find proper alpha for blend..it is close now, but probably not an exact match for vanilla
-       *  highlighting
-       */
-      GL11.glColor4f(1.f, 1.f, 1.f, 0.55f);
-      GL11.glEnable(GL11.GL_BLEND);
-      GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-      GL11.glDisable(GL11.GL_LIGHTING);
-      GL11.glDisable(GL11.GL_TEXTURE_2D);
-      GL11.glBegin(GL11.GL_QUADS);
-      GL11.glVertex2f(renderX, renderY);
-      GL11.glVertex2f(renderX, renderY+height);
-      GL11.glVertex2f(renderX+width, renderY+height);
-      GL11.glVertex2d(renderX+width, renderY);      
-      GL11.glEnd();      
-      GL11.glColor4f(1.f, 1.f, 1.f, 1.f);
-      GL11.glDisable(GL11.GL_BLEND);
-      GL11.glEnable(GL11.GL_TEXTURE_2D);
-      if(this.item!=null && this.render!=null)
+      if(highlightOnMouseOver)
+        {
+        /**
+         *  TODO -- find proper alpha for blend..it is close now, but probably not an exact match for vanilla
+         *  highlighting
+         */
+        GL11.glColor4f(1.f, 1.f, 1.f, 0.55f);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glBegin(GL11.GL_QUADS);
+        GL11.glVertex2f(renderX, renderY);
+        GL11.glVertex2f(renderX, renderY+height);
+        GL11.glVertex2f(renderX+width, renderY+height);
+        GL11.glVertex2d(renderX+width, renderY);      
+        GL11.glEnd();      
+        GL11.glColor4f(1.f, 1.f, 1.f, 1.f);
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);        
+        }
+      if(renderTooltip && this.item!=null && this.render!=null)
         {
         this.render.handleTooltipRender(item);
         }      
