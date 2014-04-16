9649cc208b9296bbfcc3227a44c99aa61d1909ac
shadowmage45 <shadowmage45@hotmail.com>
2014-04-16 14:50:29 -0600

    clean up some annoying NBT issues with container/item/synching.

--- src/main/java/net/shadowmage/ancientwarfare/core/item/ItemClickable.java
+++ src/main/java/net/shadowmage/ancientwarfare/core/item/ItemClickable.java
@@ -24,38 +24,6 @@ public boolean doesSneakBypassUse(World world, int x, int y, int z, EntityPlayer
   return false;
   }
 
-@Override
-public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity)
-  {
-  return true;
-  }
-
-@Override
-public boolean onItemUse(ItemStack stack, EntityPlayer player, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
-  {
-  return true;
-  }
-
-@Override
-public ItemStack onItemRightClick(ItemStack stack, World par2World, EntityPlayer player)
-  {
-  if(!player.worldObj.isRemote)
-    {
-    onRightClick(stack, player, getMovingObjectPositionFromPlayer(player.worldObj, player, true));
-    }
-  return stack;
-  }
-
-@Override
-public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ)
-  {
-  if(!player.worldObj.isRemote)
-    {
-    onRightClick(stack, player, getMovingObjectPositionFromPlayer(player.worldObj, player, true));    
-    }
-  return false;
-  }
-
 public void onLeftClick(ItemStack stack, EntityPlayer player, MovingObjectPosition hit)
   {
   
--- src/main/java/net/shadowmage/ancientwarfare/core/item/ItemEventHandler.java
+++ src/main/java/net/shadowmage/ancientwarfare/core/item/ItemEventHandler.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.Action;
+import net.shadowmage.ancientwarfare.core.config.AWLog;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 public class ItemEventHandler
@@ -29,6 +30,22 @@ public void onItemUse(PlayerInteractEvent evt)
         {
         item.onLeftClick(stack, player, item.getMovingObjectPositionFromPlayer(player.worldObj, player, true));        
         }    
+      }
+    }
+  else if(evt.action==Action.RIGHT_CLICK_AIR || evt.action==Action.RIGHT_CLICK_BLOCK)
+    {
+    EntityPlayer player = evt.entityPlayer;
+    ItemStack stack = evt.entityPlayer.inventory.getCurrentItem();
+    
+    if(stack==null || !(stack.getItem() instanceof ItemClickable))
+      {
+      return;
+      }
+    ItemClickable item = (ItemClickable) stack.getItem();
+    if(!player.worldObj.isRemote)
+      {
+      item.onRightClick(stack, player, item.getMovingObjectPositionFromPlayer(player.worldObj, player, true));
+      evt.setCanceled(true);
       }
     }
   }
