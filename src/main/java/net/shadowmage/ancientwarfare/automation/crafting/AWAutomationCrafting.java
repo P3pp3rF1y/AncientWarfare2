b2d3a1d787cf97d7fcbf2269e055f0bafb79e1b4
shadowmage45 <shadowmage45@hotmail.com>
2014-06-11 15:07:16 -0600

    add recipes to copy orders items

--- src/main/java/net/shadowmage/ancientwarfare/automation/crafting/AWAutomationCrafting.java
+++ src/main/java/net/shadowmage/ancientwarfare/automation/crafting/AWAutomationCrafting.java
@@ -2,10 +2,18 @@ package net.shadowmage.ancientwarfare.automation.crafting;
 
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.world.World;
 import net.shadowmage.ancientwarfare.automation.block.AWAutomationBlockLoader;
+import net.shadowmage.ancientwarfare.automation.item.AWAutomationItemLoader;
+import net.shadowmage.ancientwarfare.core.api.AWItems;
 import net.shadowmage.ancientwarfare.core.crafting.AWCraftingManager;
 import net.shadowmage.ancientwarfare.core.crafting.RecipeResearched;
+import net.shadowmage.ancientwarfare.npc.item.AWNpcItemLoader;
 
 public class AWAutomationCrafting
 {
@@ -17,91 +25,91 @@ public static void loadRecipes()
   {
   RecipeResearched recipe;
   
-  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteAutoCrafting), new Object[]{
+  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteAutoCrafting), 
       "_c_",
       "_w_",
       "_i_",
       '_', Blocks.planks,
       'c', Blocks.chest,
       'w', Blocks.crafting_table,
-      'i', Items.iron_ingot});
+      'i', Items.iron_ingot);
   recipe.addResearch("mass_production");
   
-  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteCropFarm), new Object[]{
+  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteCropFarm), 
       "___",
       "_w_",
       "_c_",
       '_', Blocks.planks,
       'w', Items.iron_hoe,
-      'c', Blocks.chest});
+      'c', Blocks.chest);
   recipe.addResearch("farming");
   
-  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteReedFarm), new Object[]{
+  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteReedFarm), 
       "___",
       "_w_",
       "_c_",
       '_', Blocks.planks,
       'w', Items.iron_shovel,
-      'c', Blocks.chest});
+      'c', Blocks.chest);
   recipe.addResearch("farming");
   
-  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteMushroomFarm), new Object[]{
+  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteMushroomFarm),
       "___",
       "_w_",
       "_c_",
       '_', Blocks.planks,
       'w', Items.wooden_shovel,
-      'c', Blocks.chest});
+      'c', Blocks.chest);
   recipe.addResearch("farming");
   
-  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteAnimalFarm), new Object[]{
+  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteAnimalFarm),
       "___",
       "_w_",
       "_c_",
       '_', Blocks.planks,
       'w', Items.iron_sword,
-      'c', Blocks.chest});
+      'c', Blocks.chest);
   recipe.addResearch("animal_husbandry");
   
-  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteQuarry), new Object[]{
+  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteQuarry), 
       "___",
       "_w_",
       "_c_",
       '_', Blocks.planks,
       'w', Items.iron_pickaxe,
-      'c', Blocks.chest});
+      'c', Blocks.chest);
   recipe.addResearch("mining");
   
-  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteForestry), new Object[]{
+  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteForestry),
       "___",
       "_w_",
       "_c_",
       '_', Blocks.planks,
       'w', Items.iron_axe,
-      'c', Blocks.chest});
+      'c', Blocks.chest);
   recipe.addResearch("construction");
   
-  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteFishFarm), new Object[]{
+  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteFishFarm), 
       "___",
       "_w_",
       "_c_",
       '_', Blocks.planks,
       'w', Items.fishing_rod,
-      'c', Blocks.chest});
+      'c', Blocks.chest);
   recipe.addResearch("fishing"); 
   
-  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteWarehouse), new Object[]{
+  recipe = AWCraftingManager.INSTANCE.addRecipe(new ItemStack(AWAutomationBlockLoader.worksiteWarehouse),
       "_p_",
       "_c_",
       "_c_",
       '_', Blocks.planks,
       'p', Items.paper,
-      'c', Blocks.chest});
+      'c', Blocks.chest);
   recipe.addResearch("trade");
+  
     
   //warehouse crafting
-  //warehouse input
-  //warehouse output
+  //warehouse interface
   //warehouse small storage
   //warehouse med storage
   //warehouse large storage  
@@ -117,5 +125,9 @@ public static void loadRecipes()
   //torque generator sterling
   //torque generator waterwheel
   //torque generator hand
+  
+  
   }
+
+
 }
--- src/main/java/net/shadowmage/ancientwarfare/npc/crafting/AWNpcCrafting.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/crafting/AWNpcCrafting.java
@@ -1,5 +1,14 @@
 package net.shadowmage.ancientwarfare.npc.crafting;
 
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+import net.shadowmage.ancientwarfare.npc.item.AWNpcItemLoader;
+
 
 public class AWNpcCrafting
 {
@@ -9,8 +18,97 @@ public class AWNpcCrafting
  */
 public static void loadRecipes()
   {
- 
-    
+  CraftingManager.getInstance().getRecipeList().add(new OrderCopyingRecipe(AWNpcItemLoader.upkeepOrder));
+  CraftingManager.getInstance().getRecipeList().add(new OrderCopyingRecipe(AWNpcItemLoader.routingOrder));
+  CraftingManager.getInstance().getRecipeList().add(new OrderCopyingRecipe(AWNpcItemLoader.combatOrder));
+  CraftingManager.getInstance().getRecipeList().add(new OrderCopyingRecipe(AWNpcItemLoader.workOrder));
   }
 
+
+private static class OrderCopyingRecipe implements IRecipe
+{
+Item item;
+private OrderCopyingRecipe(Item item){this.item=item;}
+
+@Override
+public boolean matches(InventoryCrafting var1, World var2)
+  {
+  ItemStack order1 = null, order2 = null;
+  boolean foundOtherStuff=false;
+  ItemStack stack;
+  for(int i = 0; i < var1.getSizeInventory(); i++)
+    {
+    stack = var1.getStackInSlot(i);
+    if(stack==null){continue;}
+    if(stack.getItem()==item)
+      {
+      if(order1==null){order1=stack;}
+      else if(order2==null){order2=stack;}
+      else
+        {
+        foundOtherStuff=true;
+        break;
+        }
+      }
+    else
+      {
+      foundOtherStuff=true;
+      break;
+      }
+    }
+  return !foundOtherStuff && order1!=null && order2!=null;
+  }
+
+@Override
+public ItemStack getCraftingResult(InventoryCrafting var1)
+  {
+  ItemStack order1 = null, order2 = null;
+  boolean foundOtherStuff=false;
+  ItemStack stack;
+  for(int i = 0; i < var1.getSizeInventory(); i++)
+    {
+    stack = var1.getStackInSlot(i);
+    if(stack==null){continue;}
+    if(stack.getItem()==item)
+      {
+      if(order1==null){order1=stack;}
+      else if(order2==null){order2=stack;}
+      else
+        {
+        foundOtherStuff=true;
+        break;
+        }
+      }
+    else
+      {
+      foundOtherStuff=true;
+      break;
+      }
+    }
+  if(foundOtherStuff || order1==null || order2==null){return null;}
+  ItemStack retStack = order2.copy();
+  if(order1.stackTagCompound!=null)
+    {
+    retStack.setTagCompound((NBTTagCompound)order1.stackTagCompound.copy());
+    }
+  else
+    {
+    retStack.setTagCompound(null);
+    }
+  retStack.stackSize = 2;
+  return retStack;
+  }
+
+@Override
+public int getRecipeSize()
+  {
+  return 9;
+  }
+
+@Override
+public ItemStack getRecipeOutput()
+  {
+  return null;
+  }
+}
 }
