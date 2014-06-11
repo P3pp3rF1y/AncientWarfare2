b493b45b31b06eba9df4ad1fbbbe7409fe56df54
shadowmage45 <shadowmage45@hotmail.com>
2014-06-11 09:52:42 -0600

    add inventory access button to trader gui (for player owned or when in creative mode for faction based)

--- src/main/java/net/shadowmage/ancientwarfare/npc/gui/GuiNpcTrade.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/gui/GuiNpcTrade.java
@@ -6,11 +6,13 @@ import net.shadowmage.ancientwarfare.core.container.ContainerBase;
 import net.shadowmage.ancientwarfare.core.gui.elements.Button;
 import net.shadowmage.ancientwarfare.core.gui.elements.ItemSlot;
 import net.shadowmage.ancientwarfare.core.gui.elements.Label;
+import net.shadowmage.ancientwarfare.core.network.NetworkHandler;
 import net.shadowmage.ancientwarfare.npc.container.ContainerNpcTrade;
 
 public class GuiNpcTrade extends GuiNpcBase
 {
 
+Button inventoryButton;
 Button nextButton, prevButton;
 ContainerNpcTrade tradeContainer;
 public GuiNpcTrade(ContainerBase container)
@@ -42,6 +44,15 @@ public void initElements()
       tradeContainer.nextTrade();
       refreshGui();
       }
+    }; 
+  
+  inventoryButton = new Button(8+55,8,52,12, "foo.inv")
+    {
+    @Override
+    protected void onPressed()
+      {
+      NetworkHandler.INSTANCE.openGui(player, NetworkHandler.GUI_NPC_INVENTORY, container.npc.getEntityId(), 0, 0);
+      }
     };
   }
 
@@ -51,7 +62,10 @@ public void setupElements()
   clearElements();
   addGuiElement(nextButton);
   addGuiElement(prevButton);
-  
+  if(player.capabilities.isCreativeMode || container.npc.getNpcType().equals("trader"))
+    {
+    addGuiElement(inventoryButton);    
+    }
   int wantedSlotsY = 8+12+12;
   addGuiElement(new Label(8, 21, StatCollector.translateToLocal("guistrings.wanted")));
   String label = StatCollector.translateToLocal("guistrings.for");
