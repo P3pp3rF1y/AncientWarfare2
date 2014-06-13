13046daff32e2e456afefb78812aa87a73500a3e
shadowmage45 <shadowmage45@hotmail.com>
2014-06-13 00:28:43 -0600

    more testing of rendering stuff

--- src/main/java/net/shadowmage/ancientwarfare/automation/render/RenderSterlingEngine.java
+++ src/main/java/net/shadowmage/ancientwarfare/automation/render/RenderSterlingEngine.java
@@ -2,9 +2,11 @@ package net.shadowmage.ancientwarfare.automation.render;
 
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.MathHelper;
 import net.shadowmage.ancientwarfare.core.model.ModelBaseAW;
 import net.shadowmage.ancientwarfare.core.model.ModelLoader;
 import net.shadowmage.ancientwarfare.core.util.RenderTools;
+import net.shadowmage.ancientwarfare.core.util.Trig;
 
 import org.lwjgl.opengl.GL11;
 
@@ -26,7 +28,7 @@ public RenderSterlingEngine()
 @Override
 public void renderTileEntityAt(TileEntity tile, double x, double y, double z, float partialTick)
   {
-  rotation+=0.1f;
+  rotation+=0.4f;
   GL11.glPushMatrix();
   
   RenderTools.setFullColorLightmap();
@@ -34,9 +36,51 @@ public void renderTileEntityAt(TileEntity tile, double x, double y, double z, fl
   GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0);
   
   model.getPiece("flywheel2").setRotation(0, 0, rotation);
+  calculateArmAngle(rotation);
+  
+  model.getPiece("piston_move").setPosition((pistonPos-14)*0.0625f, 0, 0);
+  
+  model.getPiece("crank_rod").setRotation(0, 0, -rotation+armAngle);
   model.renderModel();
 
   GL11.glPopMatrix();
+  }
+
+float pistonPos, armAngle;
+
+private void calculateArmAngle(float crankAngle)
+  {
+  float ra = crankAngle*Trig.TORADIANS;
+  float crankDistance = 1.f;//side a
+  float crankLength = 12.f;//side b
+  calculatePistonPosition(ra, crankDistance, crankLength);
+  calculateArmAngle(ra, crankLength, crankDistance, pistonPos);
+  }
+
+private void calculatePistonPosition(float crankAngleRadians, float radius, float length)
+  {
+  float cA = MathHelper.cos(crankAngleRadians);
+  float sA = MathHelper.sin(crankAngleRadians);
+  pistonPos = radius*cA + MathHelper.sqrt_float( length*length-radius*radius*sA*sA );
+  
+  float bx = sA * radius;
+  float by = cA * radius;
+  float cx = 0;
+  float cy = pistonPos;
+  
+  float rlrA = (float) Math.atan2(cx-bx, cy-by);
+  armAngle = rlrA*Trig.TODEGREES;
+  }
+
+private float calculateArmAngle(float radAngleA, float sideA, float sideB, float sideC)
+  {
+  if(radAngleA==0.f){return 90.f;}
+  float shortSide = sideA<sideB? sideA : sideB;
+  shortSide = sideC<shortSide? sideC : shortSide;
+  float longSide = sideA>sideB ? sideA : sideB;
+  longSide = sideC>longSide ? sideC : longSide;
+  float a = (float)((Math.asin(radAngleA)*shortSide)/longSide);    
+  return a;
   }
 
 }
