6a0aaafe0f94065aa5b6c3d82b52aa5c9c5f4554
shadowmage45 <shadowmage45@hotmail.com>
2014-06-17 21:05:51 -0600

    add translation for 'death list' to town hall gui

--- src/main/java/net/shadowmage/ancientwarfare/npc/gui/GuiTownHallInventory.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/gui/GuiTownHallInventory.java
@@ -1,6 +1,7 @@
 package net.shadowmage.ancientwarfare.npc.gui;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.StatCollector;
 import net.shadowmage.ancientwarfare.core.container.ContainerBase;
 import net.shadowmage.ancientwarfare.core.gui.GuiContainerBase;
 import net.shadowmage.ancientwarfare.core.gui.elements.Button;
@@ -22,7 +23,7 @@ public GuiTownHallInventory(ContainerBase container)
 public void initElements()
   {
   this.container.addSlots();
-  Button button = new Button(8, 8, 55, 12, "foo.deathlist")
+  Button button = new Button(8, 8, 75, 12, StatCollector.translateToLocal("guistrings.npc.death_list"))
     {
     @Override
     protected void onPressed()
