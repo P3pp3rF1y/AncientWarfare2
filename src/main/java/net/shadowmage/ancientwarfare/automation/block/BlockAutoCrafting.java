145e9e96ec17345c2e4eb80e86744f1deba9ab14
shadowmage45 <shadowmage45@hotmail.com>
2014-08-29 11:16:47 -0600

    add resources for auto-crafting station model swap -- need to figure out some rendering issues regarding worksites in general (i.e. the render path is fubar)

---
+++ src/main/java/net/shadowmage/ancientwarfare/automation/block/BlockAutoCrafting.java
@@ -0,0 +1,31 @@
+package net.shadowmage.ancientwarfare.automation.block;
+
+import net.minecraft.block.material.Material;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+import net.shadowmage.ancientwarfare.automation.tile.worksite.WorksiteAutoCrafting;
+
+public class BlockAutoCrafting extends BlockWorksiteBase
+{
+
+public BlockAutoCrafting(Material p_i45394_1_, String regName)
+  {
+  super(p_i45394_1_, regName);
+  }
+
+@Override
+public TileEntity createTileEntity(World world, int metadata)
+  {
+  return new WorksiteAutoCrafting();
+  }
+
+@Override
+public boolean shouldSideBeRendered(net.minecraft.world.IBlockAccess p_149646_1_, int p_149646_2_, int p_149646_3_, int p_149646_4_, int p_149646_5_) {return false;}
+
+@Override
+public boolean isOpaqueCube() {return false;}
+
+@Override
+public boolean isNormalCube() {return false;}
+
+}
