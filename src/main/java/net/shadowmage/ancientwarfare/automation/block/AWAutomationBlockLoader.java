1ad5aa509f1de9d4996b7c0a1d148cf1fe7c9036
shadowmage45 <shadowmage45@hotmail.com>
2014-08-29 13:50:37 -0600

    .

--- src/main/java/net/shadowmage/ancientwarfare/automation/block/AWAutomationBlockLoader.java
+++ src/main/java/net/shadowmage/ancientwarfare/automation/block/AWAutomationBlockLoader.java
@@ -39,6 +39,7 @@ import net.shadowmage.ancientwarfare.automation.tile.worksite.WorkSiteTreeFarm;
 import net.shadowmage.ancientwarfare.automation.tile.worksite.TileAutoCrafting;
 import net.shadowmage.ancientwarfare.core.block.BlockRotationHandler.RelativeSide;
 import net.shadowmage.ancientwarfare.core.item.ItemBlockOwnedRotatable;
+import net.shadowmage.ancientwarfare.core.item.ItemBlockRotatableTile;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class AWAutomationBlockLoader
@@ -269,7 +270,7 @@ public static void load()
   GameRegistry.registerBlock(warehouseStockViewer, ItemBlockOwnedRotatable.class, "warehouse_stock_viewer");
   GameRegistry.registerTileEntity(TileWarehouseStockViewer.class, "warehouse_stock_viewer_tile");
   
-  GameRegistry.registerBlock(worksiteAutoCrafting, ItemBlockOwnedRotatable.class, "civic_auto_crafting");
+  GameRegistry.registerBlock(worksiteAutoCrafting, ItemBlockRotatableTile.class, "civic_auto_crafting");
   GameRegistry.registerTileEntity(TileAutoCrafting.class, "civic_auto_crafting_tile");
   worksiteAutoCrafting.setIcon(RelativeSide.TOP, "ancientwarfare:automation/auto_crafting_top");
   worksiteAutoCrafting.setIcon(RelativeSide.FRONT, "ancientwarfare:automation/auto_crafting_front");
