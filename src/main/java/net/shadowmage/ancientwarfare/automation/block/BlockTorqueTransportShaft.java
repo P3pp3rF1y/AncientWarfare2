e86515ab09fbcf781ebc521b7ba44dcfe38508c1
shadowmage45 <shadowmage45@hotmail.com>
2014-09-29 09:03:29 -0600

    properly set bounds of torque shaft block from block orientation

--- src/main/java/net/shadowmage/ancientwarfare/automation/block/BlockTorqueTransportShaft.java
+++ src/main/java/net/shadowmage/ancientwarfare/automation/block/BlockTorqueTransportShaft.java
@@ -6,7 +6,10 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.ForgeDirection;
+import net.shadowmage.ancientwarfare.automation.tile.torque.TileTorqueTransportBase;
 import net.shadowmage.ancientwarfare.automation.tile.torque.TileTorqueTransportConduit;
 import net.shadowmage.ancientwarfare.automation.tile.torque.TileTorqueTransportShaft;
 
@@ -40,5 +43,35 @@ public void getSubBlocks(Item p_149666_1_, CreativeTabs p_149666_2_, List list)
   list.add(new ItemStack(Item.getItemFromBlock(this),1,0));
   list.add(new ItemStack(Item.getItemFromBlock(this),1,1));
   list.add(new ItemStack(Item.getItemFromBlock(this),1,2));
+  }
+
+@Override
+public void setBlockBoundsBasedOnState(IBlockAccess world, int x, int y, int z)
+  {
+  float min = 0.1875f, max = 0.8125f;
+  float x1=min, y1=min, z1=min, x2=max, y2=max, z2=max;
+  TileEntity te = world.getTileEntity(x, y, z);
+  if(te instanceof TileTorqueTransportBase)
+    {
+    TileTorqueTransportBase tile = (TileTorqueTransportBase) world.getTileEntity(x, y, z);
+    ForgeDirection d = tile.getPrimaryFacing();
+    int s1 = d.ordinal();
+    if(s1==0 || s1==1)
+      {
+      y1 = 0;
+      y2 = 1;
+      }
+    if(s1==1 || s1==2)
+      {
+      z1 = 0;
+      z2 = 1;
+      }
+    if(s1==3 || s1==4)
+      {
+      x1 = 0;
+      x2 = 1;
+      }
+    }  
+  setBlockBounds(x1, y1, z1, x2, y2, z2);
   }
 }
