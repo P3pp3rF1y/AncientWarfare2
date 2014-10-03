99e513588660b943425d95d979c4f776d10090f0
shadowmage45 <shadowmage45@hotmail.com>
2014-10-02 18:27:17 -0600

    more block texture updates

--- src/main/java/net/shadowmage/ancientwarfare/automation/block/BlockAutoCrafting.java
+++ src/main/java/net/shadowmage/ancientwarfare/automation/block/BlockAutoCrafting.java
@@ -1,7 +1,9 @@
 package net.shadowmage.ancientwarfare.automation.block;
 
 import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
 import net.shadowmage.ancientwarfare.automation.tile.worksite.TileAutoCrafting;
 
@@ -21,6 +23,9 @@ public TileEntity createTileEntity(World world, int metadata)
 
 @Override
 public boolean shouldSideBeRendered(net.minecraft.world.IBlockAccess p_149646_1_, int p_149646_2_, int p_149646_3_, int p_149646_4_, int p_149646_5_) {return false;}
+
+@Override
+public IIcon getIcon(int side, int meta){return Blocks.planks.getIcon(side, 0);}
 
 @Override
 public boolean isOpaqueCube() {return false;}
