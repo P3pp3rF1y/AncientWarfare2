204ca3d3937055d1295d081da73dd5abe7c7c269
shadowmage45 <shadowmage45@hotmail.com>
2014-10-13 13:46:37 -0600

    more prep work on vehicle for collision stuff

--- src/main/java/net/shadowmage/ancientwarfare/vehicle/entity/VehicleBase.java
+++ src/main/java/net/shadowmage/ancientwarfare/vehicle/entity/VehicleBase.java
@@ -1,16 +1,61 @@
 package net.shadowmage.ancientwarfare.vehicle.entity;
 
+import io.netty.buffer.ByteBuf;
+import cpw.mods.fml.common.registry.IEntityAdditionalSpawnData;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
+import net.shadowmage.ancientwarfare.core.config.AWLog;
 
-public class VehicleBase extends Entity
+public class VehicleBase extends Entity implements IEntityAdditionalSpawnData
 {
 
-public VehicleBase(World par1World)
+public VehicleBase(World world)
   {
-  super(par1World);
-  
+  super(world);
+  }
+
+@Override
+protected void entityInit()
+  {
+
+  }
+
+//************************************* COLLISION HANDLING *************************************//
+
+/**
+ * Bounding Box is used for collision with a solid-object entity
+ */
+@Override
+public AxisAlignedBB getBoundingBox(){return boundingBox;}
+
+@Override
+public void applyEntityCollision(Entity collider)
+  {
+  AWLog.logDebug("applyEntityCollision: "+collider);
+  super.applyEntityCollision(collider);
+  }
+
+@Override
+public void onCollideWithPlayer(EntityPlayer player)
+  {
+  AWLog.logDebug("collide with player!! "+player);
+  // TODO Auto-generated method stub
+  super.onCollideWithPlayer(player);
+  }
+
+/**
+ * Return a collision box to enable the input entity to push this entity.<br>
+ * default implementation for pushable entities is to return the input entities bounding box<br>
+ * Vehicle implementation returns null to disable pushing 
+ */
+@Override
+public AxisAlignedBB getCollisionBox(Entity entity)
+  {
+  AWLog.logDebug("getCollisionBox: "+entity);
+  return entity.boundingBox;
   }
 
 @Override
@@ -22,14 +67,10 @@ public boolean canBeCollidedWith()
 @Override
 public boolean canBePushed()
   {
-  return true;
+  return true;//this is needed for the collidedWith stuff -- will be negating the push to the vehicle while altering the pushback to the collider
   }
 
-@Override
-protected void entityInit()
-  {
-
-  }
+//************************************* NBT / NETWORK *************************************//
 
 @Override
 protected void readEntityFromNBT(NBTTagCompound var1)
@@ -40,6 +81,20 @@ protected void readEntityFromNBT(NBTTagCompound var1)
 @Override
 protected void writeEntityToNBT(NBTTagCompound var1)
   {
+  
+  }
+
+@Override
+public void writeSpawnData(ByteBuf buffer)
+  {
+  // TODO Auto-generated method stub
+  
+  }
+
+@Override
+public void readSpawnData(ByteBuf additionalData)
+  {
+  // TODO Auto-generated method stub
   
   }
 
