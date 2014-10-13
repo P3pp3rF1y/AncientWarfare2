01865ef3a20747f628e641273eaa6c31474ef9d1
shadowmage45 <shadowmage45@hotmail.com>
2014-10-13 09:55:17 -0600

    fix gate control GUI being incorrect dimensions. add translation to 'repack gate' in gate control GUI

--- src/main/java/net/shadowmage/ancientwarfare/structure/gui/GuiGateControl.java
+++ src/main/java/net/shadowmage/ancientwarfare/structure/gui/GuiGateControl.java
@@ -1,7 +1,6 @@
 package net.shadowmage.ancientwarfare.structure.gui;
 
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.StatCollector;
 import net.shadowmage.ancientwarfare.core.container.ContainerBase;
 import net.shadowmage.ancientwarfare.core.gui.GuiContainerBase;
 import net.shadowmage.ancientwarfare.core.gui.elements.Button;
@@ -11,13 +10,13 @@ public class GuiGateControl extends GuiContainerBase
 
 public GuiGateControl(ContainerBase par1Container)
   {
-  super(par1Container, 12+8+8, 55+8+8, defaultBackground);
+  super(par1Container, 55+8+8, 12+8+8, defaultBackground);
   }
 
 @Override
 public void initElements()
   {
-  Button button = new Button(8,8,55,12, StatCollector.translateToLocal("guistrings.gate.repack"))
+  Button button = new Button(8,8,55,12, "guistrings.gate.repack")
     {
     @Override
     protected void onPressed()
