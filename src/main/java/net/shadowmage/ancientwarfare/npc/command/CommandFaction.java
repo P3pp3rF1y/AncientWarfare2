6bee07a118193232b970fa70b611ff6978229a87
shadowmage45 <shadowmage45@hotmail.com>
2014-10-24 09:38:12 -0600

    finish adding faction standing manipulation command.  Closes #113

--- src/main/java/net/shadowmage/ancientwarfare/npc/command/CommandFaction.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/command/CommandFaction.java
@@ -7,8 +7,9 @@ import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.world.World;
-import net.shadowmage.ancientwarfare.core.config.AWLog;
+import net.shadowmage.ancientwarfare.npc.config.AWNPCStatics;
 import net.shadowmage.ancientwarfare.npc.faction.FactionTracker;
 
 public class CommandFaction implements ICommand
@@ -57,24 +58,51 @@ public void processCommand(ICommandSender var1, String[] var2)
   String playerName = var2[1];
   if(cmd.toLowerCase().equals("set"))
     {
-    if(var2.length<4){throw new WrongUsageException(getCommandUsage(var1), new Object[0]);}
+    if(var2.length<4){throw new WrongUsageException("command.aw.faction.set.usage", new Object[0]);}
     String faction = var2[2];
-    String amount = var2[3];    
-    AWLog.logDebug("set faction for player: "+playerName + " faction: "+faction+ " faction"+ " amount: "+amount);
+    if(!isFactionNameValid(faction)){throw new WrongUsageException("command.aw.faction.set.usage", new Object[0]);}
+    String amount = var2[3];     
+    int amt = 0;
+    try{amt = Integer.parseInt(amount);}
+    catch(NumberFormatException e){throw new WrongUsageException("command.aw.faction.set.usage", new Object[0]);}
+    FactionTracker.INSTANCE.setStandingFor(var1.getEntityWorld(), playerName, faction, amt);
+    var1.addChatMessage(new ChatComponentTranslation("command.aw.faction.set", playerName, faction, amt));
     }
   else if(cmd.toLowerCase().equals("setall"))
     {
-    if(var2.length<3){throw new WrongUsageException(getCommandUsage(var1), new Object[0]);}
+    if(var2.length<3){throw new WrongUsageException("command.aw.faction.setall.usage", new Object[0]);}
     String amount = var2[2];
-    AWLog.logDebug("setall faction for player: "+playerName + " amount: "+amount);    
+    int amt = 0;
+    try{amt = Integer.parseInt(amount);}
+    catch(NumberFormatException e){throw new WrongUsageException("command.aw.faction.setall.usage", new Object[0]);}    
+    for(String faction : AWNPCStatics.factionNames)
+      {
+      FactionTracker.INSTANCE.setStandingFor(var1.getEntityWorld(), playerName, faction, amt);
+      var1.addChatMessage(new ChatComponentTranslation("command.aw.faction.set", playerName, faction, amt));
+      }  
     }
   else if(cmd.toLowerCase().equals("get"))
     {
-    AWLog.logDebug("get faction for player: "+playerName);
     World world = var1.getEntityWorld();
-    int faction = FactionTracker.INSTANCE.getStandingFor(world, playerName, "bandit");
-    
+    var1.addChatMessage(new ChatComponentTranslation("command.aw.faction.status.player", playerName));
+    for(String faction : AWNPCStatics.factionNames)
+      {
+      int standing = FactionTracker.INSTANCE.getStandingFor(world, playerName, faction);
+      var1.addChatMessage(new ChatComponentTranslation("command.aw.faction.status.value", faction, standing));
+      }     
     }
+  }
+
+private boolean isFactionNameValid(String factionName)
+  {
+  for(String name : AWNPCStatics.factionNames)
+    {
+    if(name.equalsIgnoreCase(factionName))
+      {
+      return true;
+      }
+    }
+  return false;
   }
 
 @Override
@@ -87,24 +115,24 @@ public boolean canCommandSenderUseCommand(ICommandSender par1ICommandSender)
 @Override
 public List addTabCompletionOptions(ICommandSender var1, String[] var2)
   {
-  if(var2.length==1)
+  if(var2.length==1)//the command
     {
     return CommandBase.getListOfStringsMatchingLastWord(var2, "set", "setall", "get");
     }
-  if(var2.length==2)
+  else if(var2.length==2)//would be a player name
     {
     return CommandBase.getListOfStringsMatchingLastWord(var2, MinecraftServer.getServer().getAllUsernames());
-    }//would be a player name
-  if(var2.length==3)
+    }
+  else if(var2.length==3)//would be a faction name for the set command
     {
-    if(var2[0].toLowerCase().equals("set") || var2[0].toLowerCase().equals("get"))
+    if(var2[0].toLowerCase().equals("set"))
       {
       return CommandBase.getListOfStringsMatchingLastWord(var2, "bandit", "viking", "desert", "jungle", "pirate", "custom_1", "custom_2", "custom_3");
       }
-    else if(var2[0].toLowerCase().equals("setall"))
-      {
-      return null;
-      }
+    }
+  else if(var2.length==4)//would be a number for the set command value
+    {
+    
     }
   return null;
   }
