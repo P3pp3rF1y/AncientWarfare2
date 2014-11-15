d0cf0190686335019a0d8c2f544ea38ffb418937
shadowmage45 <shadowmage45@hotmail.com>
2014-11-14 18:02:05 -0700

    misc license stuff

--- src/main/java/net/shadowmage/ancientwarfare/structure/template/plugin/default_plugins/block_rules/TemplateRuleBlockDoors.java
+++ src/main/java/net/shadowmage/ancientwarfare/structure/template/plugin/default_plugins/block_rules/TemplateRuleBlockDoors.java
@@ -1,11 +1,11 @@
 /**
-   Copyright 2012-2013 John Cummens (aka Shadowmage, Shadowmage4513)
+   Copyright 2012-2014 John Cummens (aka Shadowmage, Shadowmage4513)
    This software is distributed under the terms of the GNU General Public License.
    Please see COPYING for precise license information.
 
-   This file is part of Ancient Warfare.
+   This file is part of Ancient Warfare 2.
 
-   Ancient Warfare is free software: you can redistribute it and/or modify
+   Ancient Warfare 2 is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
