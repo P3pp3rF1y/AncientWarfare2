4485ee17c6e9e1b84edf0fd901217168656d9818
shadowmage45 <shadowmage45@hotmail.com>
2014-12-24 09:45:20 -0700

    allow researched recipe to use ore dictionary for item matching.  -may- greatly fubar all the things. (if it does, blame whoever requested the change).  Closes #185

--- src/main/java/net/shadowmage/ancientwarfare/core/crafting/RecipeResearched.java
+++ src/main/java/net/shadowmage/ancientwarfare/core/crafting/RecipeResearched.java
@@ -3,8 +3,11 @@ package net.shadowmage.ancientwarfare.core.crafting;
 import java.util.HashSet;
 import java.util.Set;
 
+import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.ShapedRecipes;
+import net.minecraft.world.World;
+import net.minecraftforge.oredict.OreDictionary;
 import net.shadowmage.ancientwarfare.core.research.ResearchGoal;
 
 public class RecipeResearched extends ShapedRecipes
@@ -51,5 +54,60 @@ protected final RecipeResearched addResearch(int... nums)
   }
 
 public Set<Integer> getNeededResearch(){return neededResearch;}
+
+@Override
+public boolean matches(InventoryCrafting inv, World world)
+  {
+  for(int x = 0; x <= 3 - recipeWidth; x++)
+    {
+    for(int y = 0; y <= 3 - recipeHeight; ++y)
+      {
+      if(checkMatch(inv, x, y, false))
+        {
+        return true;
+        }
+      }
+    }
+  return false;
+  }
+
+private boolean checkMatch(InventoryCrafting inv, int startX, int startY, boolean mirror)
+  {
+  int width = recipeWidth;
+  int height = recipeHeight;
+  for(int x = 0; x < 3; x++)
+    {
+    for(int y = 0; y < 3; y++)
+      {
+      int subX = x - startX;
+      int subY = y - startY;
+      Object target = null;
+      if(subX >= 0 && subY >= 0 && subX < width && subY < height)
+        {
+        if(mirror)
+          {
+          target = recipeItems[width - subX - 1 + subY * width];
+          }
+        else
+          {
+          target = recipeItems[subX + subY * width];
+          }
+        }
+      ItemStack slot = inv.getStackInRowAndColumn(x, y);
+      if(target instanceof ItemStack)
+        {
+        if(!OreDictionary.itemMatches((ItemStack)target, slot, false))
+          {
+          return false;
+          }
+        }
+      else if(target == null && slot != null)
+        {
+        return false;
+        }
+      }
+    }
+  return true;
+  }
 
 }
