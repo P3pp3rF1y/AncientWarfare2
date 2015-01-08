d58563774c457e0746d7e6e56d0c6901d7cf28b1
Domochevsky <Domochevsky@users.noreply.github.com>
2015-01-09 00:34:30 +0100

    Taught them how to use QuiverBow weaponry.

--- src/main/java/net/shadowmage/ancientwarfare/npc/ai/owned/NpcAIPlayerOwnedAttackRanged.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/ai/owned/NpcAIPlayerOwnedAttackRanged.java
@@ -80,6 +80,44 @@ public void updateTask()
       {
       float pwr = (float)(attackDistance / dist);
       pwr = pwr < 0.1f ? 0.1f : pwr>1.f ? 1.f : pwr;
+      
+      // Inserting QuiverBow ranged weapon usage here (for b78+)
+      if (Loader.isModLoaded("quiverchevsky"))
+      {
+    	  if (npc.getHeldItem() != null && npc.getHeldItem().getItem() instanceof com.domochevsky.quiverbow.weapons._WeaponBase)
+    	  {
+    		  com.domochevsky.quiverbow.weapons._WeaponBase weapon = (com.domochevsky.quiverbow.weapons._WeaponBase) npc.getHeldItem().getItem();
+    		  
+    		  if (weapon.isMobUsable()) 
+    		  {  
+    			  this.npc.faceEntity(target, 30.0F, 30.0F);
+    			  
+    			  // doesn't check onUpdate of the weapon, so burst fire needs to be dealt with manually
+    			  if (weapon.doesBurstFire() && weapon.getBurstFireLeft(npc.getHeldItem()) > 0)
+				  {
+    				  weapon.doBurstFire(npc.getHeldItem(), npc.worldObj, npc);	// Firing
+					  weapon.setBurstFire(npc.getHeldItem(), weapon.getBurstFireLeft(npc.getHeldItem()) - 1);	// One less projectile on the stack
+					  
+					  if (weapon.getBurstFireLeft(npc.getHeldItem()) <= 0)	// Are you done now?
+					  {
+						  this.attackDelay = weapon.getMaxCooldown() + 1;	// Then chill out
+					  }
+					  else { this.attackDelay = 0; } // Keep firing
+				  }
+    			  else	// Regular fire
+    			  {
+    				  weapon.doSingleFire(npc.getHeldItem(), npc.worldObj, npc);	// Firing in the direction the entity is currently looking. Does not use ammo.
+    				  this.attackDelay = weapon.getMaxCooldown() + 1; 				// Hold yer horses, sonny (But only as long as the weapon needs to be ready again)
+    			  }
+    			 
+            	  return;										// We're done here
+    		  }
+    		  // else cannot be used by mobs. Nevermind.
+    	  }
+    	  // else, not holding a QB weapon. Nevermind.
+      }
+      // else, QB isn't loaded. Nevermind.
+      
       this.rangedAttacker.attackEntityWithRangedAttack(target, 1.f);
       this.attackDelay=35;
       }
