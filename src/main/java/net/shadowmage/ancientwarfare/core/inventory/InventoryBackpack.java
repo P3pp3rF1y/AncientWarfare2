972bd186775ca386529c54d8e0fad590b3ab7981
GotoLink <gotolinkminecraft@gmail.com>
2015-02-05 14:21:58 +0100

    Prevent infinite backpack storage

--- src/main/java/net/shadowmage/ancientwarfare/core/inventory/InventoryBackpack.java
+++ src/main/java/net/shadowmage/ancientwarfare/core/inventory/InventoryBackpack.java
@@ -1,6 +1,9 @@
 package net.shadowmage.ancientwarfare.core.inventory;
 
 
+import net.minecraft.item.ItemStack;
+import net.shadowmage.ancientwarfare.core.item.ItemBackpack;
+
 public class InventoryBackpack extends InventoryBasic {
 
     public InventoryBackpack(int size) {
@@ -10,5 +13,12 @@ public class InventoryBackpack extends InventoryBasic {
     @Override
     public String toString() {
         return "Backpack size: " + getSizeInventory();
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int var1, ItemStack var2) {
+        if(var2!=null && var2.getItem() instanceof ItemBackpack)
+            return false;
+        return super.isItemValidForSlot(var1, var2);
     }
 }
