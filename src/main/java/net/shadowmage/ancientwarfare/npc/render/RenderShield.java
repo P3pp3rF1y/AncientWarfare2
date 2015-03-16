e5adf95a0f5dcef876f0e2f13c77cea83dce46c6
GotoLink <gotolinkminecraft@gmail.com>
2015-03-16 17:29:49 +0100

    Shield enchantment rendering

--- src/main/java/net/shadowmage/ancientwarfare/npc/render/RenderShield.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/render/RenderShield.java
@@ -2,18 +2,21 @@ package net.shadowmage.ancientwarfare.npc.render;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.renderer.texture.TextureUtil;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.IItemRenderer;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
 
 public class RenderShield implements IItemRenderer {
 
+    private static final ResourceLocation RES_ITEM_GLINT = new ResourceLocation("textures/misc/enchanted_item_glint.png");
     public RenderShield() {
     }
 
@@ -29,29 +32,21 @@ public class RenderShield implements IItemRenderer {
 
     @Override
     public void renderItem(ItemRenderType type, ItemStack item, Object... data) {
-        EntityLivingBase entity = (EntityLivingBase) data[1];
-        render(entity, item);
+        render((EntityLivingBase) data[1], item);
     }
 
     private void render(EntityLivingBase entity, ItemStack stack) {
-        TextureManager texturemanager = Minecraft.getMinecraft().getTextureManager();
         IIcon iicon = entity.getItemIcon(stack, stack.getItemDamage());
         if (iicon == null) {
             return;
         }
         GL11.glPushMatrix();
+        TextureManager texturemanager = Minecraft.getMinecraft().getTextureManager();
         texturemanager.bindTexture(texturemanager.getResourceLocation(stack.getItemSpriteNumber()));
-//  TextureUtil.func_147950_a(false, false);//TODO wtf was/is this?
-        Tessellator tessellator = Tessellator.instance;
-        float f = iicon.getMinU();
-        float f1 = iicon.getMaxU();
-        float f2 = iicon.getMinV();
-        float f3 = iicon.getMaxV();
-        float f4 = 0.0F;
-        float f5 = 0.3F;
+        TextureUtil.func_152777_a(false, false, 1.0F);
         GL11.glEnable(GL12.GL_RESCALE_NORMAL);
 
-        GL11.glTranslatef(-f4, -f5, 0.0F);
+        GL11.glTranslatef(-0.0F, -0.3F, 0.0F);
         float f6 = 1.5F;
         GL11.glScalef(f6, f6, f6);
         GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
@@ -60,40 +55,42 @@ public class RenderShield implements IItemRenderer {
         GL11.glTranslatef(-8.f * 0.0625f, 10.f * 0.0625f, 0);
         GL11.glRotatef(-80.f, 1, 0, 0);
         GL11.glTranslatef(0, -3.f * 0.0625f, 0);
+        Tessellator tessellator = Tessellator.instance;
+        float f = iicon.getMinU();
+        float f1 = iicon.getMaxU();
+        float f2 = iicon.getMinV();
+        float f3 = iicon.getMaxV();
         ItemRenderer.renderItemIn2D(tessellator, f1, f2, f, f3, iicon.getIconWidth(), iicon.getIconHeight(), 0.0625F);
 
-
-        //TODO reenable for enchanted glint effect -- need to get a ref to the glint texture cleanly
-//  if (stack.hasEffect(stack.getItemDamage()))
-//  {
-//      GL11.glDepthFunc(GL11.GL_EQUAL);
-//      GL11.glDisable(GL11.GL_LIGHTING);
-//      texturemanager.bindTexture(RES_ITEM_GLINT);
-//      GL11.glEnable(GL11.GL_BLEND);
-//      OpenGlHelper.glBlendFunc(768, 1, 1, 0);
-//      float f7 = 0.76F;
-//      GL11.glColor4f(0.5F * f7, 0.25F * f7, 0.8F * f7, 1.0F);
-//      GL11.glMatrixMode(GL11.GL_TEXTURE);
-//      GL11.glPushMatrix();
-//      float f8 = 0.125F;
-//      GL11.glScalef(f8, f8, f8);
-//      float f9 = (float)(Minecraft.getSystemTime() % 3000L) / 3000.0F * 8.0F;
-//      GL11.glTranslatef(f9, 0.0F, 0.0F);
-//      GL11.glRotatef(-50.0F, 0.0F, 0.0F, 1.0F);
-//      ItemRenderer.renderItemIn2D(tessellator, 0.0F, 0.0F, 1.0F, 1.0F, 256, 256, 0.0625F);
-//      GL11.glPopMatrix();
-//      GL11.glPushMatrix();
-//      GL11.glScalef(f8, f8, f8);
-//      f9 = (float)(Minecraft.getSystemTime() % 4873L) / 4873.0F * 8.0F;
-//      GL11.glTranslatef(-f9, 0.0F, 0.0F);
-//      GL11.glRotatef(10.0F, 0.0F, 0.0F, 1.0F);
-//      ItemRenderer.renderItemIn2D(tessellator, 0.0F, 0.0F, 1.0F, 1.0F, 256, 256, 0.0625F);
-//      GL11.glPopMatrix();
-//      GL11.glMatrixMode(GL11.GL_MODELVIEW);
-//      GL11.glDisable(GL11.GL_BLEND);
-//      GL11.glEnable(GL11.GL_LIGHTING);
-//      GL11.glDepthFunc(GL11.GL_LEQUAL);
-//  }
+        if (stack.hasEffect(stack.getItemDamage())){
+            GL11.glDepthFunc(GL11.GL_EQUAL);
+            GL11.glDisable(GL11.GL_LIGHTING);
+            texturemanager.bindTexture(RES_ITEM_GLINT);
+            GL11.glEnable(GL11.GL_BLEND);
+            OpenGlHelper.glBlendFunc(768, 1, 1, 0);
+            float f7 = 0.76F;
+            GL11.glColor4f(0.5F * f7, 0.25F * f7, 0.8F * f7, 1.0F);
+            GL11.glMatrixMode(GL11.GL_TEXTURE);
+            GL11.glPushMatrix();
+            float f8 = 0.125F;
+            GL11.glScalef(f8, f8, f8);
+            float f9 = (float)(Minecraft.getSystemTime() % 3000L) / 3000.0F * 8.0F;
+            GL11.glTranslatef(f9, 0.0F, 0.0F);
+            GL11.glRotatef(-50.0F, 0.0F, 0.0F, 1.0F);
+            ItemRenderer.renderItemIn2D(tessellator, 0.0F, 0.0F, 1.0F, 1.0F, 256, 256, 0.0625F);
+            GL11.glPopMatrix();
+            GL11.glPushMatrix();
+            GL11.glScalef(f8, f8, f8);
+            f9 = (float)(Minecraft.getSystemTime() % 4873L) / 4873.0F * 8.0F;
+            GL11.glTranslatef(-f9, 0.0F, 0.0F);
+            GL11.glRotatef(10.0F, 0.0F, 0.0F, 1.0F);
+            ItemRenderer.renderItemIn2D(tessellator, 0.0F, 0.0F, 1.0F, 1.0F, 256, 256, 0.0625F);
+            GL11.glPopMatrix();
+            GL11.glMatrixMode(GL11.GL_MODELVIEW);
+            GL11.glDisable(GL11.GL_BLEND);
+            GL11.glEnable(GL11.GL_LIGHTING);
+            GL11.glDepthFunc(GL11.GL_LEQUAL);
+        }
 
         GL11.glDisable(GL12.GL_RESCALE_NORMAL);
         texturemanager.bindTexture(texturemanager.getResourceLocation(stack.getItemSpriteNumber()));
