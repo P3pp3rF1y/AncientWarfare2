0179c72dda86386278281293410bdaa890a364c3
CosmicDan <dconnolly.au@gmail.com>
2016-07-17 13:00:35 +1000

    New fleeing/hiding and "fear" AI
    - NPC's will run home only when they can actually see hostiles
    - If they run home and can still see hostiles, they will proceed to the town center
    - Flee timeout reduced from 400 to 50 ticks, but fear stacks with it
    - Fear level will only degrade once NPC actually stops fleeing
    - In other words, an NPC initially only runs for 2.5 seconds but if it is sent fleeing again before another 2.5 then the remainder stacks with the next flee duration, resulting in progressively longer flee/hide durations

--- src/main/java/net/shadowmage/ancientwarfare/npc/ai/NpcAIFleeHostiles.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/ai/NpcAIFleeHostiles.java
@@ -19,12 +19,14 @@ import java.util.List;
 
 public class NpcAIFleeHostiles extends NpcAI<NpcPlayerOwned> {
 
-    private static int MAX_STAY_HOME = 400, MAX_FLEE_RANGE = 16, HEIGHT_CHECK = 7, PURSUE_RANGE = 16 * 16;
+    private static int MAX_STAY_AWAY = 50, MAX_FLEE_RANGE = 16, HEIGHT_CHECK = 7, PURSUE_RANGE = 16 * 16;
     private final IEntitySelector selector;
     private final Comparator sorter;
     double distanceFromEntity = 16;
     private Vec3 fleeVector;
-    private int stayAtHomeTimer = 0;
+    private int stayOutOfSightTimer = 0;
+    private int fearLevel = 200; // fear makes NPC's wait/flee for progressively longer periods
+    boolean homeCompromised = false;
 
     public NpcAIFleeHostiles(NpcPlayerOwned npc) {
         super(npc);
@@ -47,39 +49,45 @@ public class NpcAIFleeHostiles extends NpcAI<NpcPlayerOwned> {
     @SuppressWarnings({"unchecked", "rawtypes"})
     @Override
     public boolean shouldExecute() {
-        if (!npc.getIsAIEnabled()) {
+        if (!npc.getIsAIEnabled())
             return false;
-        }
-        List list = this.npc.worldObj.selectEntitiesWithinAABB(EntityLiving.class, this.npc.boundingBox.expand(this.distanceFromEntity, 3.0D, this.distanceFromEntity), this.selector);
-        if (list.isEmpty()) {
-            return false;
-        }
-        Collections.sort(list, sorter);
-        EntityLiving fleeTarget = (EntityLiving) list.get(0);
-        // TODO
-        //if (!this.npc.canEntityBeSeen(fleeTarget))
-        //    return false;
+        // assume not fleeing, reduce fear level early
+        if (fearLevel > 0)
+            fearLevel--;
+        
         boolean flee = false;
-        if (npc.getTownHallPosition() != null || npc.hasHome()) {
-            flee = true;
-        } else {
-            fleeVector = RandomPositionGenerator.findRandomTargetBlockAwayFrom(this.npc, MAX_FLEE_RANGE, HEIGHT_CHECK, Vec3.createVectorHelper(fleeTarget.posX, fleeTarget.posY, fleeTarget.posZ));
-            if (fleeVector == null || fleeTarget.getDistanceSq(fleeVector.xCoord, fleeVector.yCoord, fleeVector.zCoord) < fleeTarget.getDistanceSqToEntity(this.npc)) {
-                flee = false;//did not find random flee-towards target, perhaps retry next tick
-            } else {
+        EntityLiving fleeTarget = getClosestVisibleHostile();
+        if (fleeTarget != null) {
+            if (npc.getTownHallPosition() != null || npc.hasHome())
                 flee = true;
+            else {
+                fleeVector = RandomPositionGenerator.findRandomTargetBlockAwayFrom(this.npc, MAX_FLEE_RANGE, HEIGHT_CHECK, Vec3.createVectorHelper(fleeTarget.posX, fleeTarget.posY, fleeTarget.posZ));
+                if (fleeVector == null || fleeTarget.getDistanceSq(fleeVector.xCoord, fleeVector.yCoord, fleeVector.zCoord) < fleeTarget.getDistanceSqToEntity(this.npc))
+                    flee = false; //did not find random flee-towards target, perhaps retry next tick
+                else
+                    flee = true;
             }
         }
-        if (flee) {
+        if (flee)
             npc.setAttackTarget(fleeTarget);
-        }
         return flee;
+    }
+    
+    private EntityLiving getClosestVisibleHostile() {
+        List list = this.npc.worldObj.selectEntitiesWithinAABB(EntityLiving.class, this.npc.boundingBox.expand(this.distanceFromEntity, 3.0D, this.distanceFromEntity), this.selector);
+        if (list.isEmpty())
+            return null;
+        Collections.sort(list, sorter);
+        EntityLiving fleeTarget = (EntityLiving) list.get(0);
+        return this.npc.canEntityBeSeen(fleeTarget) ? fleeTarget : null;
     }
 
     @Override
     public void startExecuting() {
         npc.addAITask(TASK_GO_HOME);
         npc.addAITask(TASK_FLEE);
+        stayOutOfSightTimer = MAX_STAY_AWAY + fearLevel;
+        fearLevel += MAX_STAY_AWAY;
     }
 
     /**
@@ -87,57 +95,66 @@ public class NpcAIFleeHostiles extends NpcAI<NpcPlayerOwned> {
      */
     @Override
     public boolean continueExecuting() {
-        if (!npc.getIsAIEnabled() || npc.getAttackTarget() == null || npc.getAttackTarget().isDead) {
+        if (!npc.getIsAIEnabled() || npc.getAttackTarget() == null || npc.getAttackTarget().isDead)
             return false;
-        }
-        return stayAtHomeTimer != 0 || npc.getAttackTarget().getDistanceSqToEntity(this.npc) < distanceFromEntity * distanceFromEntity;
+        return stayOutOfSightTimer != 0 || npc.getAttackTarget().getDistanceSqToEntity(this.npc) < distanceFromEntity * distanceFromEntity;
     }
 
     @Override
     public void updateTask() {
         if (npc.getAttackTarget() == null || npc.getAttackTarget().isDead) {
             npc.setAttackTarget(null);
-            stayAtHomeTimer = 0;
+            stayOutOfSightTimer = 0;
             return;
         }
         BlockPosition pos = null;
-        double distSq;
-        if (npc.getTownHallPosition() != null) {
-            pos = npc.getTownHallPosition();
-            distSq = npc.getDistanceSq(pos);
-        } else if (npc.hasHome()) {
+        double distSq = 0;
+        
+        if (npc.hasHome()) {
             distSq = npc.getDistanceSqFromHome();
             ChunkCoordinates cc = npc.getHomePosition();
             pos = new BlockPosition(cc.posX, cc.posY, cc.posZ);
-        } else
-        {
-            if(fleeVector == null){
-                return;
-            }//check distance to flee vector
-            distSq = npc.getDistanceSq(fleeVector.xCoord, fleeVector.yCoord, fleeVector.zCoord);
-            if (distSq > MIN_RANGE) {
-                moveToPosition(fleeVector.xCoord, fleeVector.yCoord, fleeVector.zCoord, distSq);
-            } else {
-                if (npc.getDistanceSqToEntity(npc.getAttackTarget()) < PURSUE_RANGE)//entity still chasing, find a new flee vector
-                {
-                    fleeVector = RandomPositionGenerator.findRandomTargetBlockAwayFrom(this.npc, MAX_FLEE_RANGE, HEIGHT_CHECK, Vec3.createVectorHelper(npc.getAttackTarget().posX, npc.getAttackTarget().posY, npc.getAttackTarget().posZ));
-                    if (fleeVector == null) {
-                        npc.setAttackTarget(null);//retry next tick..perhaps...
-                    }
-                } else//entity too far to care, stop running
-                {
-                    npc.setAttackTarget(null);
+            if (distSq < MIN_RANGE) {
+                // NPC is home, check for visible hostiles
+                EntityLiving fleeTarget = getClosestVisibleHostile();
+                if (fleeTarget != null) {
+                    homeCompromised = true;
+                    npc.addAITask(TASK_ALARM);
                 }
             }
         }
-        if (pos!=null && distSq > MIN_RANGE) {
-            moveToPosition(pos, distSq);
-            stayAtHomeTimer = MAX_STAY_HOME;
-        }else {
-            if (stayAtHomeTimer > 0) {
-                stayAtHomeTimer--;
+        
+        if ((homeCompromised || !npc.hasHome()) && npc.getTownHallPosition() != null) {
+            pos = npc.getTownHallPosition();
+            distSq = npc.getDistanceSq(pos);
+            // TODO: Check if within range of town hall and somehow defend it/themselves from present hostiles
+        }
+
+        if (pos == null) {
+            if (fleeVector == null)
+                return;
+            //check distance to flee vector
+            distSq = npc.getDistanceSq(fleeVector.xCoord, fleeVector.yCoord, fleeVector.zCoord);
+            if (distSq > MIN_RANGE)
+                moveToPosition(fleeVector.xCoord, fleeVector.yCoord, fleeVector.zCoord, distSq);
+            else {
+                if (npc.getDistanceSqToEntity(npc.getAttackTarget()) < PURSUE_RANGE) {//entity still chasing, find a new flee vector 
+                    fleeVector = RandomPositionGenerator.findRandomTargetBlockAwayFrom(this.npc, MAX_FLEE_RANGE, HEIGHT_CHECK, Vec3.createVectorHelper(npc.getAttackTarget().posX, npc.getAttackTarget().posY, npc.getAttackTarget().posZ));
+                    if (fleeVector == null)
+                        npc.setAttackTarget(null);//retry next tick..perhaps...
+                } else // entity too far to care, stop running
+                    npc.setAttackTarget(null);
             }
         }
+        
+        // keep running home
+        if (pos!=null && distSq > MIN_RANGE) {
+            moveToPosition(pos, distSq);
+        }
+        if (stayOutOfSightTimer > 0)
+            stayOutOfSightTimer--;
+        else
+            resetTask();
     }
 
     @Override
@@ -145,7 +162,8 @@ public class NpcAIFleeHostiles extends NpcAI<NpcPlayerOwned> {
         fleeVector = null;
         npc.getNavigator().clearPathEntity();
         npc.setAttackTarget(null);
-        npc.removeAITask(TASK_GO_HOME + TASK_FLEE);
+        npc.removeAITask(TASK_GO_HOME + TASK_FLEE + TASK_ALARM);
+        homeCompromised = false;
     }
 
 }
