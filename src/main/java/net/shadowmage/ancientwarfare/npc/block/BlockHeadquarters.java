a0e24d99f53128ca2bc34e0dd8c0f7226d9f4dbc
CosmicDan <dconnolly.au@gmail.com>
2017-02-28 00:03:32 +1100

    Reset HQ back to Town Hall when captured by another player

--- src/main/java/net/shadowmage/ancientwarfare/npc/block/BlockHeadquarters.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/block/BlockHeadquarters.java
@@ -5,9 +5,13 @@ import java.util.ArrayList;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import net.shadowmage.ancientwarfare.core.interop.ModAccessors;
+import net.shadowmage.ancientwarfare.npc.tile.TileTownHall;
 
 public class BlockHeadquarters extends BlockTownHall {
 
@@ -27,5 +31,22 @@ public class BlockHeadquarters extends BlockTownHall {
         ArrayList<ItemStack> drops = new ArrayList<ItemStack>();
         drops.add(new ItemStack(Item.getItemFromBlock(AWNPCBlockLoader.townHall)));
         return drops;
+    }
+    
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int sideHit, float hitX, float hitY, float hitZ) {
+        if (!player.worldObj.isRemote) {
+            TileEntity te = world.getTileEntity(x, y, z);
+            String currentOwnerName = ((TileTownHall) te).getOwnerName();
+            if (!player.getCommandSenderName().equals(currentOwnerName)) {
+                // different player to the owner has used the town hall
+                if (!ModAccessors.FTBU.areFriends(player.getCommandSenderName(), currentOwnerName)) {
+                    // new player is NOT a friend, change this HQ back to a town hall block
+                    world.setBlock(x, y, z, AWNPCBlockLoader.townHall, 0, 3);
+                    return world.getBlock(x, y, z).onBlockActivated(world, x, y, z, player, sideHit, hitX, hitY, hitZ);
+                }
+            }
+        }
+        return super.onBlockActivated(world, x, y, z, player, sideHit, hitX, hitY, hitZ);
     }
 }
