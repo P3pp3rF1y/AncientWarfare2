9e22ad850e21ea5fc190693c68734a73ed3b0f58
CosmicDan <dconnolly.au@gmail.com>
2017-03-11 20:39:48 +1100

    HarderWildlife skeleton classes in API

---
+++ src/api/java/com/draco18s/wildlife/entity/CowStats.java
@@ -0,0 +1,31 @@
+package com.draco18s.wildlife.entity;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+import net.minecraftforge.common.IExtendedEntityProperties;
+
+/*
+ * Skeleton class
+ */
+public class CowStats implements IExtendedEntityProperties {
+
+    @Override
+    public void saveNBTData(NBTTagCompound compound) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void loadNBTData(NBTTagCompound compound) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void init(Entity entity, World world) {
+        // TODO Auto-generated method stub
+        
+    }
+	
+}
