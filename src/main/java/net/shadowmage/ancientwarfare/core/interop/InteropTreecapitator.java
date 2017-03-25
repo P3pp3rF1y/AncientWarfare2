4e2abe18e8cc35b91d97bd2e8a969953c39d8bef
CosmicDan <dconnolly.au@gmail.com>
2017-03-26 01:20:40 +1100

    Dedicated server crash fix

--- src/main/java/net/shadowmage/ancientwarfare/core/interop/InteropTreecapitator.java
+++ src/main/java/net/shadowmage/ancientwarfare/core/interop/InteropTreecapitator.java
@@ -1,9 +1,5 @@
 package net.shadowmage.ancientwarfare.core.interop;
 
-import java.util.UUID;
-
-import com.mojang.authlib.GameProfile;
-
 import net.minecraft.block.Block;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -20,7 +16,6 @@ public class InteropTreecapitator extends InteropTreecapitatorDummy {
     public void doTreecapitate(World world, Block block, int meta, int posX, int posY, int posZ) {
         FakePlayer fakePlayer = FakePlayerFactory.get(MinecraftServer.getServer().worldServerForDimension(world.provider.dimensionId), AncientWarfareCore.gameProfile);
         fakePlayer.inventory.setInventorySlotContents(0, new ItemStack(Items.diamond_axe));
-        fakePlayer.inventory.changeCurrentItem(0);
         new bspkrs.treecapitator.forge.ForgeEventHandler().onBlockHarvested(new BreakEvent(posX, posY, posZ, world, block, meta, fakePlayer));
     }
 }
