78b71fbe380c32a32f3281d1cd6e5cc12c5d79ba
P3pp3rF1y <p3pp3rf1y@gmail.com>
2017-10-06 17:31:36 +0200

    Made packets execute as scheduled tasks (given that packets are handled on separate thread)

--- src/main/java/net/shadowmage/ancientwarfare/core/network/PacketHandlerServer.java
+++ src/main/java/net/shadowmage/ancientwarfare/core/network/PacketHandlerServer.java
@@ -1,6 +1,9 @@
 package net.shadowmage.ancientwarfare.core.network;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.FMLNetworkEvent;
 import net.shadowmage.ancientwarfare.core.AncientWarfareCore;
@@ -9,11 +12,16 @@ public class PacketHandlerServer {
 
     @SubscribeEvent
     public void onServerPacket(FMLNetworkEvent.ServerCustomPacketEvent evt) {
-        PacketBase.readPacket(evt.getPacket().payload()).execute(((NetHandlerPlayServer) evt.getHandler()).player);
+        PacketBase packet = PacketBase.readPacket(evt.getPacket().payload());
+        EntityPlayer player  = ((NetHandlerPlayServer) evt.getHandler()).player;
+
+        ((WorldServer) player.world).addScheduledTask(() -> packet.execute(player));
     }
 
     @SubscribeEvent
     public void onClientPacket(FMLNetworkEvent.ClientCustomPacketEvent evt) {
-        PacketBase.readPacket(evt.getPacket().payload()).execute(AncientWarfareCore.proxy.getClientPlayer());
+        PacketBase packet = PacketBase.readPacket(evt.getPacket().payload());
+
+        Minecraft.getMinecraft().addScheduledTask(() -> packet.execute(AncientWarfareCore.proxy.getClientPlayer()));
     }
 }
