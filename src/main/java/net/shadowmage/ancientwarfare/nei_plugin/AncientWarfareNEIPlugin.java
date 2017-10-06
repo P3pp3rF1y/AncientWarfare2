39aa3b30549eec2bcbd1212e9e83dbef695125b5
P3pp3rF1y <p3pp3rf1y@gmail.com>
2017-10-06 15:35:23 +0200

    just commented out the rest of NEI plugin code, will be replaced with JEI code in the future

--- src/main/java/net/shadowmage/ancientwarfare/nei_plugin/AncientWarfareNEIPlugin.java
+++ src/main/java/net/shadowmage/ancientwarfare/nei_plugin/AncientWarfareNEIPlugin.java
@@ -1,34 +1,34 @@
 //TODO JEI integration
-package net.shadowmage.ancientwarfare.nei_plugin;
-
-import net.minecraftforge.fml.common.Loader;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.shadowmage.ancientwarfare.core.config.AWLog;
-
-@Mod
-        (
-                name = "Ancient Warfare NEI Plugin",
-                modid = "AncientWarfareNEIPlugin",
-                version = "@VERSION@",
-                dependencies = "required-after:ancientwarfare;after:notenoughitems"
-        )
-public class AncientWarfareNEIPlugin {
-
-    @EventHandler
-    public void init(FMLInitializationEvent evt) {
-        if (Loader.isModLoaded("NotEnoughItems")) {
-            if (evt.getSide().isClient()) {
-                AWLog.log("NEI Detected, attempting load of NEI Plugin");
-                try {
-                    Class.forName("net.shadowmage.ancientwarfare.nei_plugin.AWNeiRecipeHandler").newInstance();
-                    AWLog.log("NEI Plugin loaded successfully");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        } else
-            AWLog.log("Skipping loading of NEI plugin, NEI not found!");
-    }
-}
+//package net.shadowmage.ancientwarfare.nei_plugin;
+//
+//import net.minecraftforge.fml.common.Loader;
+//import net.minecraftforge.fml.common.Mod;
+//import net.minecraftforge.fml.common.Mod.EventHandler;
+//import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+//import net.shadowmage.ancientwarfare.core.config.AWLog;
+//
+//@Mod
+//        (
+//                name = "Ancient Warfare NEI Plugin",
+//                modid = "AncientWarfareNEIPlugin",
+//                version = "@VERSION@",
+//                dependencies = "required-after:ancientwarfare;after:notenoughitems"
+//        )
+//public class AncientWarfareNEIPlugin {
+//
+//    @EventHandler
+//    public void init(FMLInitializationEvent evt) {
+//        if (Loader.isModLoaded("NotEnoughItems")) {
+//            if (evt.getSide().isClient()) {
+//                AWLog.log("NEI Detected, attempting load of NEI Plugin");
+//                try {
+//                    Class.forName("net.shadowmage.ancientwarfare.nei_plugin.AWNeiRecipeHandler").newInstance();
+//                    AWLog.log("NEI Plugin loaded successfully");
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        } else
+//            AWLog.log("Skipping loading of NEI plugin, NEI not found!");
+//    }
+//}
