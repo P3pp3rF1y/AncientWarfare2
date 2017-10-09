fc262cdfccbafd634e18c6fc9e54317b35836742
P3pp3rF1y <p3pp3rf1y@gmail.com>
2017-10-09 12:49:40 +0200

    And actually changing the flag to 2 as 0 doesn't send updates to client and thus we end up with big parts of structures not displaying properly without reload.

--- src/main/java/net/shadowmage/ancientwarfare/structure/template/build/StructureBuilder.java
+++ src/main/java/net/shadowmage/ancientwarfare/structure/template/build/StructureBuilder.java
@@ -129,7 +129,7 @@ public class StructureBuilder implements IStructureBuilder {
         if (pos.getY() <= 0 || pos.getY() >= world.getHeight()) {
             return;
         }
-        world.setBlockState(pos, block.getStateFromMeta(meta), 0);
+        world.setBlockState(pos, block.getStateFromMeta(meta), 2);
         //TODO this used to be more complicated for perf reasons - look into whether needs to be recreated
 //        Chunk chunk = world.getChunkFromBlockCoords(x, z);
 //        ExtendedBlockStorage stc = chunk.getBlockStorageArray()[y >> 4];
