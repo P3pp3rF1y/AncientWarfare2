12f01b66ec045c9fb47cb611959dc4668527a05b
P3pp3rF1y <p3pp3rf1y@gmail.com>
2017-12-12 00:38:27 +0100

    small NPE fix for when FTBU isn't available and no owner is assigned to ownable machine/npc

--- src/main/java/net/shadowmage/ancientwarfare/core/compat/ftb/FTBUCompatDummy.java
+++ src/main/java/net/shadowmage/ancientwarfare/core/compat/ftb/FTBUCompatDummy.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 public class FTBUCompatDummy implements IFTBUCompat {
     @Override
     public boolean areTeamMates(UUID player1, UUID player2) {
-        return player1.equals(player2);
+        return player1 != null && player1.equals(player2);
     }
 
     @Override
