12c03273f02752a8f56f0e4bdb273e6a7fe922fc
P3pp3rF1y <p3pp3rf1y@gmail.com>
2018-01-06 00:58:18 +0100

    npc container inventory converted to item handler

--- src/main/java/net/shadowmage/ancientwarfare/core/inventory/IInventorySaveable.java
+++
@@ -1,9 +0,0 @@
-package net.shadowmage.ancientwarfare.core.inventory;
-
-import net.minecraft.inventory.IInventory;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraftforge.common.util.INBTSerializable;
-
-public interface IInventorySaveable extends IInventory, INBTSerializable<NBTTagCompound> {
-
-}
