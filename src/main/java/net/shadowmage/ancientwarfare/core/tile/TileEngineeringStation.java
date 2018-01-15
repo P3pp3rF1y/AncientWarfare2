1cb0cfa192a07612eed5bf479e7f7c055d99392b
P3pp3rF1y <p3pp3rf1y@gmail.com>
2018-01-15 22:16:08 +0100

    engineering station item handler

--- src/main/java/net/shadowmage/ancientwarfare/core/tile/TileEngineeringStation.java
+++ src/main/java/net/shadowmage/ancientwarfare/core/tile/TileEngineeringStation.java
@@ -3,7 +3,6 @@ package net.shadowmage.ancientwarfare.core.tile;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.IInventoryChangedListener;
 import net.minecraft.inventory.InventoryCraftResult;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
@@ -12,151 +11,156 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.NonNullList;
 import net.minecraft.world.World;
+import net.minecraftforge.items.ItemStackHandler;
 import net.shadowmage.ancientwarfare.core.block.BlockRotationHandler.IRotatableTile;
 import net.shadowmage.ancientwarfare.core.crafting.AWCraftingManager;
-import net.shadowmage.ancientwarfare.core.inventory.InventoryBasic;
 import net.shadowmage.ancientwarfare.core.item.ItemResearchBook;
 import net.shadowmage.ancientwarfare.core.util.BlockTools;
 import net.shadowmage.ancientwarfare.core.util.InventoryTools;
 
 import javax.annotation.Nonnull;
 
-public class TileEngineeringStation extends TileUpdatable implements IRotatableTile, IInventoryChangedListener {
+public class TileEngineeringStation extends TileUpdatable implements IRotatableTile {
 
-    EnumFacing facing = EnumFacing.NORTH;
-    NonNullList<ItemStack> matrixShadow;
+	EnumFacing facing = EnumFacing.NORTH;
+	NonNullList<ItemStack> matrixShadow;
 
-    public InventoryCrafting layoutMatrix;
-    public InventoryCraftResult result;
-    public InventoryBasic bookInventory;
-    public InventoryBasic extraSlots;
+	public InventoryCrafting layoutMatrix;
+	public InventoryCraftResult result;
+	public final ItemStackHandler bookInventory;
+	public final ItemStackHandler extraSlots;
 
-    public TileEngineeringStation() {
-        Container c = new Container() {
-            @Override
-            public boolean canInteractWith(EntityPlayer var1) {
-                return true;
-            }
+	public TileEngineeringStation() {
+		Container c = new Container() {
+			@Override
+			public boolean canInteractWith(EntityPlayer var1) {
+				return true;
+			}
 
-            @Override
-            public void onCraftMatrixChanged(IInventory par1iInventory) {
-                onInventoryChanged(null);
-            }
-        };
-        layoutMatrix = new InventoryCrafting(c, 3, 3);
-        matrixShadow = NonNullList.withSize(layoutMatrix.getSizeInventory(), ItemStack.EMPTY);
-        bookInventory = new InventoryBasic(1, this);
-        result = new InventoryCraftResult();
-        extraSlots = new InventoryBasic(18, this);
-    }
+			@Override
+			public void onCraftMatrixChanged(IInventory par1iInventory) {
+				onInventoryChanged();
+			}
+		};
+		layoutMatrix = new InventoryCrafting(c, 3, 3);
+		matrixShadow = NonNullList.withSize(layoutMatrix.getSizeInventory(), ItemStack.EMPTY);
+		bookInventory = new ItemStackHandler(1) {
+			@Override
+			protected void onContentsChanged(int slot) {
+				onInventoryChanged();
+			}
+		};
 
-    public String getCrafterName() {
-        return ItemResearchBook.getResearcherName(bookInventory.getStackInSlot(0));
-    }
+		result = new InventoryCraftResult();
+		extraSlots = new ItemStackHandler(18) {
+			@Override
+			protected void onContentsChanged(int slot) {
+				markDirty();
+			}
+		};
+	}
 
-    /*
-     * called to shadow a copy of the input matrix, to know what to refill
-     */
-    public void preItemCrafted() {
-        @Nonnull ItemStack stack;
-        for (int i = 0; i < layoutMatrix.getSizeInventory(); i++) {
-            stack = layoutMatrix.getStackInSlot(i);
-            matrixShadow.set(i, stack.isEmpty() ? ItemStack.EMPTY : stack.copy());
-        }
-    }
+	public String getCrafterName() {
+		return ItemResearchBook.getResearcherName(bookInventory.getStackInSlot(0));
+	}
 
-    public void onItemCrafted() {
-        @Nonnull ItemStack layoutStack;
-        for (int i = 0; i < layoutMatrix.getSizeInventory(); i++) {
-            layoutStack = matrixShadow.get(i);
-            if (layoutStack.isEmpty()) {
-                continue;
-            }
-            if (!layoutMatrix.getStackInSlot(i).isEmpty()) {
-                continue;
-            }
-            layoutMatrix.setInventorySlotContents(i, InventoryTools.removeItems(extraSlots,  null, layoutStack, 1));
-        }
-    }
+	/*
+	 * called to shadow a copy of the input matrix, to know what to refill
+	 */
+	public void preItemCrafted() {
+		@Nonnull ItemStack stack;
+		for(int i = 0; i < layoutMatrix.getSizeInventory(); i++) {
+			stack = layoutMatrix.getStackInSlot(i);
+			matrixShadow.set(i, stack.isEmpty() ? ItemStack.EMPTY : stack.copy());
+		}
+	}
 
-    private void onLayoutMatrixChanged() {
-        result.setInventorySlotContents(0, AWCraftingManager.findMatchingRecipe(layoutMatrix, world, getCrafterName()));
-        result.setRecipeUsed(CraftingManager.findMatchingRecipe(layoutMatrix, world));
-    }
+	public void onItemCrafted() {
+		@Nonnull ItemStack layoutStack;
+		for(int i = 0; i < layoutMatrix.getSizeInventory(); i++) {
+			layoutStack = matrixShadow.get(i);
+			if(layoutStack.isEmpty()) {
+				continue;
+			}
+			if(!layoutMatrix.getStackInSlot(i).isEmpty()) {
+				continue;
+			}
+			layoutMatrix.setInventorySlotContents(i, InventoryTools.removeItems(extraSlots, layoutStack, 1));
+		}
+	}
 
-    @Override
-    public void onInventoryChanged(IInventory internal){
-        onLayoutMatrixChanged();
-        markDirty();
-    }
+	private void onLayoutMatrixChanged() {
+		result.setInventorySlotContents(0, AWCraftingManager.findMatchingRecipe(layoutMatrix, world, getCrafterName()));
+		result.setRecipeUsed(CraftingManager.findMatchingRecipe(layoutMatrix, world));
+	}
 
-    @Override
-    public void setWorld(World world){
-        super.setWorld(world);
-        onLayoutMatrixChanged();
-    }
+	private void onInventoryChanged() {
+		onLayoutMatrixChanged();
+		markDirty();
+	}
 
-    @Override
-    protected void writeUpdateNBT(NBTTagCompound tag) {
-        super.writeUpdateNBT(tag);
-        tag.setInteger("facing", facing.ordinal());
-    }
+	@Override
+	public void setWorld(World world) {
+		super.setWorld(world);
+		onLayoutMatrixChanged();
+	}
 
-    @Override
-    protected void handleUpdateNBT(NBTTagCompound tag) {
-        super.handleUpdateNBT(tag);
-        facing = EnumFacing.VALUES[tag.getInteger("facing")];
-        BlockTools.notifyBlockUpdate(this);
-    }
+	@Override
+	protected void writeUpdateNBT(NBTTagCompound tag) {
+		super.writeUpdateNBT(tag);
+		tag.setInteger("facing", facing.ordinal());
+	}
 
-    @Override
-    public void readFromNBT(NBTTagCompound tag) {
-        super.readFromNBT(tag);
-        InventoryTools.readInventoryFromNBT(bookInventory, tag.getCompoundTag("bookInventory"));
-        InventoryTools.readInventoryFromNBT(extraSlots, tag.getCompoundTag("extraInventory"));
-        InventoryTools.readInventoryFromNBT(result, tag.getCompoundTag("resultInventory"));
-        InventoryTools.readInventoryFromNBT(layoutMatrix, tag.getCompoundTag("layoutMatrix"));
-        facing = EnumFacing.values()[tag.getInteger("facing")];
-    }
+	@Override
+	protected void handleUpdateNBT(NBTTagCompound tag) {
+		super.handleUpdateNBT(tag);
+		facing = EnumFacing.VALUES[tag.getInteger("facing")];
+		BlockTools.notifyBlockUpdate(this);
+	}
 
-    @Override
-    public NBTTagCompound writeToNBT(NBTTagCompound tag) {
-        super.writeToNBT(tag);
+	@Override
+	public void readFromNBT(NBTTagCompound tag) {
+		super.readFromNBT(tag);
+		bookInventory.deserializeNBT(tag.getCompoundTag("bookInventory"));
+		extraSlots.deserializeNBT(tag.getCompoundTag("extraInventory"));
+		InventoryTools.readInventoryFromNBT(result, tag.getCompoundTag("resultInventory"));
+		InventoryTools.readInventoryFromNBT(layoutMatrix, tag.getCompoundTag("layoutMatrix"));
+		facing = EnumFacing.values()[tag.getInteger("facing")];
+	}
 
-        NBTTagCompound inventoryTag = new NBTTagCompound();
-        InventoryTools.writeInventoryToNBT(bookInventory, inventoryTag);
-        tag.setTag("bookInventory", inventoryTag);
+	@Override
+	public NBTTagCompound writeToNBT(NBTTagCompound tag) {
+		super.writeToNBT(tag);
 
-        inventoryTag = new NBTTagCompound();
-        InventoryTools.writeInventoryToNBT(extraSlots, inventoryTag);
-        tag.setTag("extraInventory", inventoryTag);
+		tag.setTag("bookInventory", bookInventory.serializeNBT());
+		tag.setTag("extraInventory", extraSlots.serializeNBT());
 
-        inventoryTag = new NBTTagCompound();
-        InventoryTools.writeInventoryToNBT(result, inventoryTag);
-        tag.setTag("resultInventory", inventoryTag);
+		NBTTagCompound inventoryTag = new NBTTagCompound();
+		InventoryTools.writeInventoryToNBT(result, inventoryTag);
+		tag.setTag("resultInventory", inventoryTag);
 
-        inventoryTag = new NBTTagCompound();
-        InventoryTools.writeInventoryToNBT(layoutMatrix, inventoryTag);
-        tag.setTag("layoutMatrix", inventoryTag);
+		inventoryTag = new NBTTagCompound();
+		InventoryTools.writeInventoryToNBT(layoutMatrix, inventoryTag);
+		tag.setTag("layoutMatrix", inventoryTag);
 
-        tag.setInteger("facing", facing.ordinal());
-        return tag;
-    }
+		tag.setInteger("facing", facing.ordinal());
+		return tag;
+	}
 
-    @Override
-    public EnumFacing getPrimaryFacing() {
-        return facing;
-    }
+	@Override
+	public EnumFacing getPrimaryFacing() {
+		return facing;
+	}
 
-    @Override
-    public void setPrimaryFacing(EnumFacing face) {
-        this.facing = face;
-        BlockTools.notifyBlockUpdate(this);
-    }
+	@Override
+	public void setPrimaryFacing(EnumFacing face) {
+		this.facing = face;
+		BlockTools.notifyBlockUpdate(this);
+	}
 
-    public void onBlockBreak(){
-        InventoryTools.dropInventoryInWorld(world, bookInventory, pos);
-        InventoryTools.dropInventoryInWorld(world, extraSlots, pos);
-        InventoryTools.dropInventoryInWorld(world, layoutMatrix, pos);
-    }
+	public void onBlockBreak() {
+		InventoryTools.dropInventoryInWorld(world, bookInventory, pos);
+		InventoryTools.dropInventoryInWorld(world, extraSlots, pos);
+		InventoryTools.dropInventoryInWorld(world, layoutMatrix, pos);
+	}
 }
