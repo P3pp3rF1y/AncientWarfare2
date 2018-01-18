49cc33b819bb229142ff116ba8349d3661a833ee
P3pp3rF1y <p3pp3rf1y@gmail.com>
2018-01-18 16:26:53 +0100

    Some Iterables to streams

--- src/main/java/net/shadowmage/ancientwarfare/npc/proxy/NpcCommonProxy.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/proxy/NpcCommonProxy.java
@@ -1,7 +1,6 @@
 package net.shadowmage.ancientwarfare.npc.proxy;
 
 import com.google.common.base.Supplier;
-import com.google.common.collect.Iterables;
 import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 import net.minecraft.nbt.NBTTagCompound;
@@ -14,48 +13,49 @@ import java.io.InputStream;
 import java.util.HashSet;
 
 public class NpcCommonProxy extends CommonProxyBase {
-    private HashSet<GameProfile> profileCache = new HashSet<>();
+	private HashSet<GameProfile> profileCache = new HashSet<>();
 
-    public void loadSkins() {
-    }
+	public void loadSkins() {
+	}
 
-    public ResourceLocation loadSkinPackImage(String packName, String imageName, InputStream is) {
-        return null;
-    }
+	public ResourceLocation loadSkinPackImage(String packName, String imageName, InputStream is) {
+		return null;
+	}
 
-    public GameProfile getProfile(final String name) {
-        return profileCache.stream().filter(input -> input.getName().equals(name)).findFirst().orElseGet((Supplier<GameProfile>) () -> null);
-    }
+	public GameProfile getProfile(final String name) {
+		return profileCache.stream().filter(input -> input.getName().equals(name)).findFirst().orElseGet((Supplier<GameProfile>) () -> null);
+	}
 
-    public void cacheProfile(GameProfile gameprofile) {
-        profileCache.add(gameprofile);
-    }
+	public void cacheProfile(GameProfile gameprofile) {
+		profileCache.add(gameprofile);
+	}
 
-    public NBTTagCompound cacheProfile(WorldServer worldServer, final String name) {
-        GameProfile gameprofile = getProfile(name);
-        if (gameprofile == null) {
-            gameprofile = worldServer.getMinecraftServer().getPlayerProfileCache().getGameProfileForUsername(name);
-            if (gameprofile != null) {
-                Property property = Iterables.getFirst(gameprofile.getProperties().get("textures"), null);
-                if (property == null) {
-                    gameprofile = worldServer.getMinecraftServer().getMinecraftSessionService().fillProfileProperties(gameprofile, true);
-                }
-                cacheProfile(gameprofile);
-            }
-        }
-        if(gameprofile!=null) {
-            NBTTagCompound tagCompound = new NBTTagCompound();
-            try {
-                NBTUtil.writeGameProfile(tagCompound, gameprofile);
-            }catch (Throwable handled){
-                return null;
-            }
-            return tagCompound;
-        }
-        return null;
-    }
+	public NBTTagCompound cacheProfile(WorldServer worldServer, final String name) {
+		GameProfile gameprofile = getProfile(name);
+		if (gameprofile == null) {
+			gameprofile = worldServer.getMinecraftServer().getPlayerProfileCache().getGameProfileForUsername(name);
+			if (gameprofile != null) {
+				Property property = gameprofile.getProperties().get("textures").stream().findFirst().orElse(null);
+				if (property == null) {
+					gameprofile = worldServer.getMinecraftServer().getMinecraftSessionService().fillProfileProperties(gameprofile, true);
+				}
+				cacheProfile(gameprofile);
+			}
+		}
+		if (gameprofile != null) {
+			NBTTagCompound tagCompound = new NBTTagCompound();
+			try {
+				NBTUtil.writeGameProfile(tagCompound, gameprofile);
+			}
+			catch (Throwable handled) {
+				return null;
+			}
+			return tagCompound;
+		}
+		return null;
+	}
 
-    public ResourceLocation getPlayerSkin(String name){
-        return null;
-    }
+	public ResourceLocation getPlayerSkin(String name) {
+		return null;
+	}
 }
