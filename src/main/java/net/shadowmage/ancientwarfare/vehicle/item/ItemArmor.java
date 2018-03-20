a413d924af65d1895942a068d7c6c565c1cbee46
P3pp3rF1y <p3pp3rf1y@gmail.com>
2018-03-20 17:54:03 +0100

    vehicle armor item rendering

--- src/main/java/net/shadowmage/ancientwarfare/vehicle/item/ItemArmor.java
+++ src/main/java/net/shadowmage/ancientwarfare/vehicle/item/ItemArmor.java
@@ -1,5 +1,6 @@
 package net.shadowmage.ancientwarfare.vehicle.item;
 
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.ItemStack;
@@ -7,6 +8,8 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.shadowmage.ancientwarfare.core.AncientWarfareCore;
+import net.shadowmage.ancientwarfare.core.util.ModelLoaderHelper;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -24,5 +27,11 @@ public class ItemArmor extends ItemBaseVehicle {
 	@SideOnly(Side.CLIENT)
 	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
 		tooltip.add(I18n.format(tooltipName));
+	}
+
+	@Override
+	public void registerClient() {
+		ModelLoaderHelper.registerItem(this, (i, m) -> new ModelResourceLocation(new ResourceLocation(AncientWarfareCore.modID, "vehicle/armor"),
+				"variant=" + getRegistryName().getResourcePath()));
 	}
 }
