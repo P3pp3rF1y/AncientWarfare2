91ed9978bc3034e317829f77c55ddbf849488679
P3pp3rF1y <p3pp3rf1y@gmail.com>
2018-05-06 21:18:25 +0200

    Adding missing package info

---
+++ src/main/java/net/shadowmage/ancientwarfare/automation/compat/agricraft/package-info.java
@@ -0,0 +1,8 @@
+// Auto generated package-info by MCP
+
+@ParametersAreNonnullByDefault @MethodsReturnNonnullByDefault
+package net.shadowmage.ancientwarfare.automation.compat.agricraft;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
