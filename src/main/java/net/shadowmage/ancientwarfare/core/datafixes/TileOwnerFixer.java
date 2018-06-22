8882c99cc8d08f8bc44a4bdf6c55bac1a4daf3ff
p3pp3rf1y <p3pp3rf1y@gmail.com>
2018-06-22 15:48:31 +0200

    added logging of bad uuid string to tile owner fixer

--- src/main/java/net/shadowmage/ancientwarfare/core/datafixes/TileOwnerFixer.java
+++ src/main/java/net/shadowmage/ancientwarfare/core/datafixes/TileOwnerFixer.java
@@ -2,6 +2,7 @@ package net.shadowmage.ancientwarfare.core.datafixes;
 
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.datafix.IFixableData;
+import net.shadowmage.ancientwarfare.core.AncientWarfareCore;
 
 import java.util.UUID;
 
@@ -17,35 +18,43 @@ public class TileOwnerFixer implements IFixableData {
 	@Override
 	public NBTTagCompound fixTagCompound(NBTTagCompound compound) {
 		String id = compound.getString("id");
-		switch (id) {
-			case "minecraft:hand_cranked_generator_tile":
-				compound.setString(NEW_OWNER_NAME_TAG, compound.getString("owner"));
-				break;
-			case "minecraft:town_hall_tile":
-			case "minecraft:auto_crafting_tile":
-			case "minecraft:warehouse_control_tile":
-			case "minecraft:quarry_tile":
-			case "minecraft:forestry_tile":
-			case "minecraft:crop_farm_tile":
-			case "minecraft:fruit_farm_tile":
-			case "minecraft:mushroom_farm_tile":
-			case "minecraft:animal_farm_tile":
-			case "minecraft:fish_farm_tile":
-			case "minecraft:reed_farm_tile":
-				compound.setString(NEW_OWNER_NAME_TAG, compound.getString("owner"));
-				compound.setUniqueId(NEW_OWNER_ID_TAG, UUID.fromString(compound.getString("ownerId")));
-				break;
-			case "minecraft:research_station_tile":
-				compound.setString(NEW_OWNER_NAME_TAG, compound.getString("owningPlayer"));
-				compound.setUniqueId(NEW_OWNER_ID_TAG, UUID.fromString(compound.getString("ownerId")));
-				break;
-			case "minecraft:mailbox_tile":
-			case "minecraft:structure_builder_ticked_tile":
-			case "minecraft:warehouse_stock_viewer_tile":
-				compound.setUniqueId(NEW_OWNER_ID_TAG, UUID.fromString(compound.getString("ownerId")));
-				break;
-			default:
+		try {
+			switch (id) {
+				case "minecraft:hand_cranked_generator_tile":
+					compound.setString(NEW_OWNER_NAME_TAG, compound.getString("owner"));
+					break;
+				case "minecraft:town_hall_tile":
+				case "minecraft:auto_crafting_tile":
+				case "minecraft:warehouse_control_tile":
+				case "minecraft:quarry_tile":
+				case "minecraft:forestry_tile":
+				case "minecraft:crop_farm_tile":
+				case "minecraft:fruit_farm_tile":
+				case "minecraft:mushroom_farm_tile":
+				case "minecraft:animal_farm_tile":
+				case "minecraft:fish_farm_tile":
+				case "minecraft:reed_farm_tile":
+					compound.setString(NEW_OWNER_NAME_TAG, compound.getString("owner"));
+					compound.setUniqueId(NEW_OWNER_ID_TAG, UUID.fromString(compound.getString("ownerId")));
+					break;
+				case "minecraft:research_station_tile":
+					compound.setString(NEW_OWNER_NAME_TAG, compound.getString("owningPlayer"));
+					compound.setUniqueId(NEW_OWNER_ID_TAG, UUID.fromString(compound.getString("ownerId")));
+					break;
+				case "minecraft:mailbox_tile":
+				case "minecraft:structure_builder_ticked_tile":
+				case "minecraft:warehouse_stock_viewer_tile":
+					compound.setUniqueId(NEW_OWNER_ID_TAG, UUID.fromString(compound.getString("ownerId")));
+					break;
+				default:
 
+			}
+		}
+		catch (IllegalArgumentException e) {
+			int x = compound.getInteger("x");
+			int y = compound.getInteger("y");
+			int z = compound.getInteger("z");
+			AncientWarfareCore.log.error("Tile {} at x:{} y:{} z:{} has invalid owner UUID and will need to be removed.", id, x, y, z);
 		}
 		return compound;
 	}
