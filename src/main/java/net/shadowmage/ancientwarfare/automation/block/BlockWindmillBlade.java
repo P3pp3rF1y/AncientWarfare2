af759b87e58403776ea64edc6da4360e351525bf
P3pp3rF1y <p3pp3rf1y@gmail.com>
2018-07-23 23:03:16 +0200

    stopped snow from accumulating on top of windmill blades.

--- src/main/java/net/shadowmage/ancientwarfare/automation/block/BlockWindmillBlade.java
+++ src/main/java/net/shadowmage/ancientwarfare/automation/block/BlockWindmillBlade.java
@@ -7,6 +7,7 @@ import codechicken.lib.model.bakery.ModelBakery;
 import codechicken.lib.model.bakery.generation.IBakery;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockFaceShape;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
@@ -91,6 +92,11 @@ public class BlockWindmillBlade extends BlockBaseAutomation implements IBakeryPr
 	@Override
 	public boolean isFullCube(IBlockState state) {
 		return false;
+	}
+
+	@Override
+	public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
+		return BlockFaceShape.UNDEFINED;
 	}
 
 	@Override
