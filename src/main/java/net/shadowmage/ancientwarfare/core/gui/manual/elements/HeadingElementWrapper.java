54036488a0dc35fd581622fc9684e3e75a9b1f81
P3pp3rF1y <p3pp3rf1y@gmail.com>
2018-08-02 22:24:24 +0200

    Fixed heading display outside of dev

--- src/main/java/net/shadowmage/ancientwarfare/core/gui/manual/elements/HeadingElementWrapper.java
+++ src/main/java/net/shadowmage/ancientwarfare/core/gui/manual/elements/HeadingElementWrapper.java
@@ -1,6 +1,7 @@
 package net.shadowmage.ancientwarfare.core.gui.manual.elements;
 
 import com.google.common.collect.ImmutableList;
+import net.minecraft.util.text.TextFormatting;
 import net.shadowmage.ancientwarfare.core.gui.manual.GuiManual;
 import net.shadowmage.ancientwarfare.core.gui.manual.IElementWrapperCreator;
 import net.shadowmage.ancientwarfare.core.manual.HeadingElement;
@@ -19,8 +20,8 @@ public class HeadingElementWrapper extends BaseElementWrapper<HeadingElement> {
 	@Override
 	public void render(int mouseX, int mouseY, float partialTick) {
 		super.render(mouseX, mouseY, partialTick);
-		String prefix = getElement().getLevel() == 1 ? "§l" : "§n";
-		ClientProxy.getUnicodeFontRenderer().drawSplitString(prefix + getElement().getText() + "§r", renderX, renderY + TOP_PADDING, width, 0x000000);
+		TextFormatting prefix = getElement().getLevel() == 1 ? TextFormatting.BOLD : TextFormatting.UNDERLINE;
+		ClientProxy.getUnicodeFontRenderer().drawSplitString(prefix + getElement().getText() + TextFormatting.RESET, renderX, renderY + TOP_PADDING, width, 0x000000);
 	}
 
 	@Override
