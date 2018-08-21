05b174fe84fac7d89baafae32676a1723a5ce751
DaemonUmbra <daemon.umbra@gmail.com>
2018-08-21 18:41:27 -0400

    Added the ability for NPCs to function in the Twilight Forest Dimension
    Added a world time check that will run instad of !isDayTime when an NPC is in the Twilight Forest dimension, expanded the CompatLoader slightly

--- src/main/java/net/shadowmage/ancientwarfare/core/compat/CompatLoader.java
+++ src/main/java/net/shadowmage/ancientwarfare/core/compat/CompatLoader.java
@@ -2,18 +2,26 @@ package net.shadowmage.ancientwarfare.core.compat;
 
 import net.minecraftforge.fml.common.Loader;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CompatLoader {
-	private static Set<ICompat> compats = new HashSet<>();
+	private static Map<String,ICompat> compats = new HashMap<>();
 
 	public static void registerCompat(ICompat compat) {
-		compats.add(compat);
+		compats.put(compat.getModId(),compat);
+	}
+
+	public static boolean isCompatRegistered(String modId){
+		return compats.containsKey(modId);
+	}
+
+	public static ICompat getCompat(String modId){
+		return compats.get(modId);
 	}
 
 	public static void init() {
-		for (ICompat compat : compats) {
+		for (ICompat compat : compats.values()) {
 			if (Loader.isModLoaded(compat.getModId())) {
 				compat.init();
 			}
