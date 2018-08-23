1e7c8d6224b470dc0d4577cfe28d1423d3d7b573
P3pp3rF1y <p3pp3rf1y@gmail.com>
2018-08-23 23:08:35 +0200

    Tuned sound block to run and track sounds client side to simplify usage. Also added a right click clears search field to make changes to search words easier

--- src/main/java/net/shadowmage/ancientwarfare/structure/tile/TileSoundBlock.java
+++ src/main/java/net/shadowmage/ancientwarfare/structure/tile/TileSoundBlock.java
@@ -2,28 +2,30 @@ package net.shadowmage.ancientwarfare.structure.tile;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.audio.ISound;
+import net.minecraft.client.audio.PositionedSoundRecord;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ITickable;
-import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.SoundEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.shadowmage.ancientwarfare.core.interfaces.ISinger;
 import net.shadowmage.ancientwarfare.core.tile.TileUpdatable;
 import net.shadowmage.ancientwarfare.core.util.BlockTools;
 import net.shadowmage.ancientwarfare.core.util.SongPlayData;
 import net.shadowmage.ancientwarfare.structure.init.AWStructureBlocks;
 
-import java.util.List;
+import javax.annotation.Nullable;
+import java.util.Optional;
 
 public class TileSoundBlock extends TileUpdatable implements ISinger, ITickable {
-
-	private boolean playing = false;//if currently playing a tune.
-	private boolean redstoneInteraction = false;
-	private int currentDelay;//the current cooldown delay.  if not playing, this delay will be incremented before attempting to start next song
-	private int tuneIndex = -1;//the index of the song being played / to play, incremented/updated on songStart()
-	private int playerCheckDelay;//used to not check for players -every- tick. checks every 10 ticks
+	private int currentDelay;
+	private int tuneIndex = -1;
+	private PositionedSoundRecord currentTune = null;
+	private int playerCheckDelay;
 	private int playerRange = 20;
-	private int playTime;//tracking current play time.  when this exceeds length, cooldown delay is triggered
 	private SongPlayData tuneData;
 	private IBlockState disguiseState;
 
@@ -33,123 +35,84 @@ public class TileSoundBlock extends TileUpdatable implements ISinger, ITickable 
 
 	@Override
 	public void update() {
-		if (world.isRemote) {
+		if (!world.isRemote) {
 			return;
 		}
-		if (playing) {
-			if (playTime-- <= 0) {
-				endSong();
-			}
-		} else {
-			if (currentDelay-- <= 0) {
-				if (tuneData.getPlayOnPlayerEntry()) {
-					if (playerCheckDelay-- <= 0) {
-						playerCheckDelay = 20;
-						AxisAlignedBB aabb = new AxisAlignedBB(pos, pos.add(1, 1, 1)).expand(playerRange, playerRange, playerRange);
 
-						List<EntityPlayer> list = world.getEntitiesWithinAABB(EntityPlayer.class, aabb);
-						if (list != null && !list.isEmpty()) {
-							startSong();
-						}
-					}
-				} else if (isRedstoneInteraction()) {
-					if (world.getStrongPower(pos) > 0) {
+		if (!validateAndGetPlaying() && tuneData.size() > 0 && currentDelay-- <= 0) {
+			if (tuneData.getPlayOnPlayerEntry()) {
+				if (playerCheckDelay-- <= 0) {
+					playerCheckDelay = 20;
+					if (Minecraft.getMinecraft().player.getDistance(pos.getX() + 0.5D, pos.getY() + 0.5D, pos.getZ() + 0.5D) <= playerRange) {
 						startSong();
 					}
-				} else {
-					startSong();
 				}
+			} else {
+				startSong();
 			}
 		}
 	}
 
+	private Optional<PositionedSoundRecord> getCurrentTune() {
+		return Optional.ofNullable(currentTune);
+	}
+
+	@SideOnly(Side.CLIENT)
 	private void startSong() {
 		if (tuneData.getIsRandom()) {
 			tuneIndex = 0;
 			if (tuneData.size() > 0) {
 				tuneIndex = world.rand.nextInt(tuneData.size());
+
+				setCurrentTune(tuneData.get(tuneIndex).getSound());
 			}
 		} else {
-			tuneIndex++;
+			tuneIndex = tuneIndex + 1 < tuneData.size() ? tuneIndex + 1 : 0;
+			setCurrentTune(tuneData.get(tuneIndex).getSound());
 		}
-		if (tuneIndex >= tuneData.size()) {
-			tuneIndex = 0;
-		}
-		if (tuneData.size() <= 0) {
-			return;
-		}
-		playing = true;
-		playTime = tuneData.get(tuneIndex).play(world, pos);
-	}
-
-	private void endSong() {
-		playing = false;
-		playTime = 0;
-		currentDelay = tuneData.getMinDelay();
-		int diff = tuneData.getMaxDelay() - currentDelay;
-		if (diff > 0) {
-			currentDelay += world.rand.nextInt(diff);
-		}
+		getCurrentTune().ifPresent(t -> Minecraft.getMinecraft().getSoundHandler().playSound(t));
 	}
 
 	@Override
 	protected void writeUpdateNBT(NBTTagCompound tag) {
 		super.writeUpdateNBT(tag);
-		cacheToNBT(tag);
+		writeToNBT(tag);
 	}
 
 	@Override
 	protected void handleUpdateNBT(NBTTagCompound tag) {
 		super.handleUpdateNBT(tag);
-		cacheFromNBT(tag);
-	}
-
-	private void cacheFromNBT(NBTTagCompound tag) {
-		String id = tag.getString("block");
-		if (!id.isEmpty()) {
-			disguiseState = Block.getBlockFromName(id).getStateFromMeta(tag.getInteger("meta"));
-		}
-	}
-
-	private void cacheToNBT(NBTTagCompound tag) {
-		if (disguiseState != null) {
-			tag.setString("block", disguiseState.getBlock().getRegistryName().toString());
-			tag.setInteger("meta", disguiseState.getBlock().getMetaFromState(disguiseState));
-		}
+		readFromNBT(tag);
 	}
 
 	@Override
 	public void readFromNBT(NBTTagCompound tag) {
 		super.readFromNBT(tag);
 		tuneData.readFromNBT(tag.getCompoundTag("tuneData"));
-		redstoneInteraction = tag.getBoolean("redstone");
 		tuneIndex = tag.getInteger("tuneIndex");
 		playerRange = tag.getInteger("range");
-		cacheFromNBT(tag);
+		String id = tag.getString("block");
+		if (!id.isEmpty()) {
+			disguiseState = Block.getBlockFromName(id).getStateFromMeta(tag.getInteger("meta"));
+		}
 	}
 
 	@Override
 	public NBTTagCompound writeToNBT(NBTTagCompound tag) {
 		super.writeToNBT(tag);
 		tag.setTag("tuneData", tuneData.writeToNBT(new NBTTagCompound()));
-		tag.setBoolean("redstone", redstoneInteraction);
 		tag.setInteger("tuneIndex", tuneIndex);
 		tag.setInteger("range", playerRange);
-		cacheToNBT(tag);
+		if (disguiseState != null) {
+			tag.setString("block", disguiseState.getBlock().getRegistryName().toString());
+			tag.setInteger("meta", disguiseState.getBlock().getMetaFromState(disguiseState));
+		}
 
 		return tag;
 	}
 
 	public SongPlayData getSongs() {
 		return tuneData;
-	}
-
-	public boolean isRedstoneInteraction() {
-		return redstoneInteraction;
-	}
-
-	public void setRedstoneInteraction(boolean redstoneInteraction) {
-		this.redstoneInteraction = redstoneInteraction;
 	}
 
 	public void setPlayerRange(int value) {
@@ -172,5 +135,42 @@ public class TileSoundBlock extends TileUpdatable implements ISinger, ITickable 
 			world.notifyNeighborsRespectDebug(pos, this.blockType, true);
 			markDirty();
 		}
+	}
+
+	@SideOnly(Side.CLIENT)
+	@SuppressWarnings("squid:S3655") //exiting early if tune not present just Sonar is not able to figure that out
+	private boolean validateAndGetPlaying() {
+		if (!getCurrentTune().isPresent()) {
+			return false;
+		}
+		ISound positionedsoundrecord = getCurrentTune().get();
+		boolean isPlaying = Minecraft.getMinecraft().getSoundHandler().isSoundPlaying(positionedsoundrecord);
+		if (!isPlaying) {
+			resetCurrentTune();
+		}
+
+		return isPlaying;
+	}
+
+	private void resetCurrentTune() {
+		tuneIndex = -1;
+		currentTune = null;
+		int diff = Math.abs(tuneData.getMaxDelay() - tuneData.getMinDelay());
+		currentDelay = tuneData.getMinDelay() + diff > 0 ? world.rand.nextInt(diff) : 0;
+	}
+
+	@SideOnly(Side.CLIENT)
+	private void setCurrentTune(@Nullable SoundEvent currentTune) {
+		if (currentTune != null) {
+			this.currentTune = PositionedSoundRecord.getRecordSoundRecord(currentTune, (float) pos.getX(), (float) pos.getY(), (float) pos.getZ());
+		}
+	}
+
+	@Override
+	public void invalidate() {
+		if (world.isRemote) {
+			getCurrentTune().ifPresent(t -> Minecraft.getMinecraft().getSoundHandler().stopSound(t));
+		}
+		super.invalidate();
 	}
 }
