f18909c0dfde0c1466648709154508293de4a673
P3pp3rF1y <p3pp3rf1y@gmail.com>
2019-01-04 14:51:04 +0100

    Added a limitation to not be able to open chests in structures when there are still non passive npcs in the structure.

--- src/main/java/net/shadowmage/ancientwarfare/npc/event/EventHandler.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/event/EventHandler.java
@@ -1,20 +1,33 @@
 package net.shadowmage.ancientwarfare.npc.event;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAITasks.EntityAITaskEntry;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.shadowmage.ancientwarfare.core.gamedata.AWGameData;
 import net.shadowmage.ancientwarfare.npc.entity.NpcBase;
 import net.shadowmage.ancientwarfare.npc.entity.NpcPlayerOwned;
 import net.shadowmage.ancientwarfare.npc.entity.faction.NpcFaction;
 import net.shadowmage.ancientwarfare.npc.registry.FactionRegistry;
 import net.shadowmage.ancientwarfare.npc.registry.NpcDefaultsRegistry;
+import net.shadowmage.ancientwarfare.structure.gamedata.StructureMap;
+import net.shadowmage.ancientwarfare.structure.init.AWStructureBlocks;
+import net.shadowmage.ancientwarfare.structure.worldgen.StructureEntry;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
 
@@ -64,4 +77,36 @@ public class EventHandler {
 		}
 	}
 
+	@SubscribeEvent
+	public void onChestClicked(PlayerInteractEvent.RightClickBlock evt) {
+		World world = evt.getWorld();
+		BlockPos pos = evt.getPos();
+		EntityPlayer player = evt.getEntityPlayer();
+		if (!player.capabilities.isCreativeMode && !player.isSneaking() && isChest(world, pos)) {
+			getStructure(world, pos).ifPresent(structure -> {
+				for (NpcFaction factionNpc : world.getEntitiesWithinAABB(NpcFaction.class, structure.getBB().getAABB())) {
+					if (!factionNpc.isPassive()) {
+						evt.setCanceled(true);
+						evt.setCancellationResult(EnumActionResult.FAIL);
+						player.sendStatusMessage(new TextComponentTranslation("gui.ancientwarfarenpc.no_chest_access", factionNpc.getFaction()), true);
+					}
+				}
+			});
+		}
+	}
+
+	private Optional<StructureEntry> getStructure(World world, BlockPos pos) {
+		StructureMap map = AWGameData.INSTANCE.getData(world, StructureMap.class);
+		for (StructureEntry structure : map.getEntriesNear(world, pos.getX(), pos.getZ(), 1, true, new ArrayList<>())) {
+			if (structure.getBB().contains(pos)) {
+				return Optional.of(structure);
+			}
+		}
+		return Optional.empty();
+	}
+
+	private boolean isChest(World world, BlockPos pos) {
+		Block block = world.getBlockState(pos).getBlock();
+		return block == Blocks.CHEST || block == AWStructureBlocks.ADVANCED_LOOT_CHEST;
+	}
 }
