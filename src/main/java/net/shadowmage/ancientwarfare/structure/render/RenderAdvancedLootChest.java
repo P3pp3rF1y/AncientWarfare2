d6962faf46ddf4cee95f4c9bbaa33e3d5b68355d
P3pp3rF1y <p3pp3rf1y@gmail.com>
2019-01-13 23:04:54 +0100

    advanced loot chest number of rolls and loot table name set to only display in creative or to spectators

--- src/main/java/net/shadowmage/ancientwarfare/structure/render/RenderAdvancedLootChest.java
+++ src/main/java/net/shadowmage/ancientwarfare/structure/render/RenderAdvancedLootChest.java
@@ -1,8 +1,10 @@
 package net.shadowmage.ancientwarfare.structure.render;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.tileentity.TileEntityChestRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.world.GameType;
 import net.shadowmage.ancientwarfare.structure.tile.TileAdvancedLootChest;
 
 public class RenderAdvancedLootChest extends TileEntitySpecialRenderer<TileAdvancedLootChest> {
@@ -10,7 +12,9 @@ public class RenderAdvancedLootChest extends TileEntitySpecialRenderer<TileAdvan
 
 	@Override
 	public void render(TileAdvancedLootChest te, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
-		super.render(te, x, y, z, partialTicks, destroyStage, alpha);
+		if (Minecraft.getMinecraft().player.capabilities.isCreativeMode || Minecraft.getMinecraft().playerController.getCurrentGameType() == GameType.SPECTATOR) {
+			super.render(te, x, y, z, partialTicks, destroyStage, alpha);
+		}
 
 		chestRenderer.render(te, x, y, z, partialTicks, destroyStage, alpha);
 	}
