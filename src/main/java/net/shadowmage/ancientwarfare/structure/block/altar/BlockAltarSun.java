3ebd85b9473bcca76b73a316610e98a23c2daeb2
P3pp3rF1y <p3pp3rf1y@gmail.com>
2019-02-02 19:18:53 +0100

    Altar sun particles

--- src/main/java/net/shadowmage/ancientwarfare/structure/block/altar/BlockAltarSun.java
+++ src/main/java/net/shadowmage/ancientwarfare/structure/block/altar/BlockAltarSun.java
@@ -3,6 +3,7 @@ package net.shadowmage.ancientwarfare.structure.block.altar;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
@@ -12,6 +13,9 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.shadowmage.ancientwarfare.structure.render.ParticleSun;
+
+import java.util.Random;
 
 import static net.shadowmage.ancientwarfare.core.render.property.CoreProperties.FACING;
 
@@ -47,19 +51,36 @@ public class BlockAltarSun extends BlockAltarTop {
 	@Override
 	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
 		switch (state.getValue(FACING)) {
-			case NORTH:
-			case SOUTH:
-				return AABB_NORTH_SOUTH;
 			case WEST:
 			case EAST:
 				return AABB_WEST_EAST;
+			case NORTH:
+			case SOUTH:
+			default:
+				return AABB_NORTH_SOUTH;
 		}
-		return AABB_NORTH_SOUTH;
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
 	public boolean canRenderInLayer(IBlockState state, BlockRenderLayer layer) {
 		return layer == BlockRenderLayer.CUTOUT;
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void randomDisplayTick(IBlockState state, World world, BlockPos pos, Random rand) {
+		int maxParticles = world.rand.nextInt(3);
+		for (int i = 0; i < maxParticles; i++) {
+			double angle = world.rand.nextFloat() * Math.PI * 2d;
+			float distance = world.rand.nextFloat() * 5 / 16f;
+			double sunHorizontalOffset = 0.5d + Math.sin(angle) * distance;
+			double distanceFromSun = 0.5d + (world.rand.nextBoolean() ? 0.1d : -0.1d);
+			double yOffset = 0.55d + Math.cos(angle) * distance;
+			EnumFacing.Axis facingAxis = state.getValue(FACING).getAxis();
+			double xOffset = facingAxis == EnumFacing.Axis.X ? distanceFromSun : sunHorizontalOffset;
+			double zOffset = facingAxis == EnumFacing.Axis.Z ? distanceFromSun : sunHorizontalOffset;
+			Minecraft.getMinecraft().effectRenderer.addEffect(new ParticleSun(world, pos.getX() + xOffset, pos.getY() + yOffset, pos.getZ() + zOffset));
+		}
 	}
 }
