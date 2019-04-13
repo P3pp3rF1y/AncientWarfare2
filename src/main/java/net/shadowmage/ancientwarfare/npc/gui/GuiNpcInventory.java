53163c70bdb609f68bc4c243a40a32f0230c11ff
ThatPolishKid99 <sebichrzan99@gmail.com>
2019-04-13 15:32:10 -0500

    Added Stand Ground NPC Option

--- src/main/java/net/shadowmage/ancientwarfare/npc/gui/GuiNpcInventory.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/gui/GuiNpcInventory.java
@@ -5,6 +5,7 @@ import net.minecraft.item.ItemStack;
 import net.shadowmage.ancientwarfare.core.container.ContainerBase;
 import net.shadowmage.ancientwarfare.core.gui.GuiContainerBase;
 import net.shadowmage.ancientwarfare.core.gui.elements.Button;
+import net.shadowmage.ancientwarfare.core.gui.elements.Checkbox;
 import net.shadowmage.ancientwarfare.core.gui.elements.ItemSlot;
 import net.shadowmage.ancientwarfare.core.gui.elements.Label;
 import net.shadowmage.ancientwarfare.core.gui.elements.Text;
@@ -16,9 +17,9 @@ import net.shadowmage.ancientwarfare.npc.entity.NpcCombat;
 import net.shadowmage.ancientwarfare.npc.init.AWNPCItems;
 
 public class GuiNpcInventory extends GuiContainerBase<ContainerNpcInventory> {
-
 	private Text nameInput;
-
+	Button button;
+	private Checkbox doNotPursueCheckbox;
 	private static int buttonX = 8 + 18 + 18 + 18 + 18 + 4;
 
 	public GuiNpcInventory(ContainerBase container) {
@@ -50,8 +51,6 @@ public class GuiNpcInventory extends GuiContainerBase<ContainerNpcInventory> {
 			}
 		};
 		addGuiElement(textureInput);
-
-		Button button;
 
 		if (getContainer().entity instanceof NpcBase) {
 
@@ -112,6 +111,16 @@ public class GuiNpcInventory extends GuiContainerBase<ContainerNpcInventory> {
 			addGuiElement(button);
 		}
 
+		if (getContainer().isArcher) {
+			doNotPursueCheckbox = new Checkbox(buttonX, 108, 12, 12, "guistrings.npc.donotpursue") {
+				@Override
+				public void onToggled() {
+					getContainer().doNotPursue = checked();
+				}
+			};
+			addGuiElement(doNotPursueCheckbox);
+		}
+
 		ItemSlot slot;
 		boolean isCombatNpc = getContainer().entity instanceof NpcCombat;
 
@@ -149,14 +158,19 @@ public class GuiNpcInventory extends GuiContainerBase<ContainerNpcInventory> {
 	}
 
 	@Override
-	protected boolean onGuiCloseRequested() {
-		getContainer().setName();
-		return super.onGuiCloseRequested();
+	public void setupElements() {
+		nameInput.setText(getContainer().entity.getCustomNameTag());
+		if (getContainer().isArcher) {
+			doNotPursueCheckbox.setChecked(getContainer().doNotPursue);
+		}
 	}
 
 	@Override
-	public void setupElements() {
-		nameInput.setText(getContainer().entity.getCustomNameTag());
+	protected boolean onGuiCloseRequested() {
+		getContainer().sendChangesToServer();
+		getContainer().setName();
+		return super.onGuiCloseRequested();
+
 	}
 
 }
