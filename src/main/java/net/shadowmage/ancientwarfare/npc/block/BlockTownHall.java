a4b4418e6305d97b4cba75176be66e9f9c49766f
ThatPolishKid99 <sebichrzan99@gmail.com>
2019-07-15 19:28:47 -0500

    Town Hall Retexture

--- src/main/java/net/shadowmage/ancientwarfare/npc/block/BlockTownHall.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/block/BlockTownHall.java
@@ -5,10 +5,13 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.shadowmage.ancientwarfare.core.util.WorldTools;
 import net.shadowmage.ancientwarfare.npc.tile.TileTownHall;
 
@@ -33,6 +36,15 @@ public class BlockTownHall extends BlockBaseNPC {
 	public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
 		return WorldTools.clickInteractableTileWithHand(world, pos, player, hand);
 	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public BlockRenderLayer getBlockLayer() {
+		return BlockRenderLayer.CUTOUT;
+	}
+
+	@Override
+	public boolean isOpaqueCube(IBlockState iBlockState) { return false; }
 
 	/*
 	 * Lets the block know when one of its neighbor changes. Doesn't know which neighbor changed (coordinates passed are
