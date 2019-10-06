ae99582102198a694c826a81fcd8a2b59c69a930
WinDanesz <win.danesz@users.noreply.github.com>
2019-10-06 12:17:31 +0100

    faction priests throws potions at players (which can't hurt them or their faction members)

--- src/main/java/net/shadowmage/ancientwarfare/npc/ai/faction/NpcAIFactionPotionAttack.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/ai/faction/NpcAIFactionPotionAttack.java
@@ -1,20 +1,18 @@
 package net.shadowmage.ancientwarfare.npc.ai.faction;
 
 import net.minecraft.entity.IRangedAttackMob;
-import net.minecraft.entity.SharedMonsterAttributes;
-import net.shadowmage.ancientwarfare.npc.ai.NpcAIAttack;
 import net.shadowmage.ancientwarfare.npc.entity.NpcBase;
 
-public class NpcAIFactionRangedAttack extends NpcAIAttack<NpcBase> {
+public class NpcAIFactionPotionAttack extends NpcAIFactionRangedAttack {
 
 	private final IRangedAttackMob rangedAttacker;
 
 	private int attackDistanceSq = -1;
 
-	public NpcAIFactionRangedAttack(NpcBase npc) {
+	public NpcAIFactionPotionAttack(NpcBase npc) {
 		super(npc);
 		this.rangedAttacker = (IRangedAttackMob) npc;//will classcastexception if improperly used..
-		this.moveSpeed = 1.d;
+		this.moveSpeed = 0.8d;
 	}
 
 	@Override
@@ -22,10 +20,10 @@ public class NpcAIFactionRangedAttack extends NpcAIAttack<NpcBase> {
 		return (dist > getAttackDistanceSq() || !this.npc.getEntitySenses().canSee(this.getTarget()));
 	}
 
+	@Override
 	protected double getAttackDistanceSq() {
 		if (attackDistanceSq == -1) {
-			double distance = npc.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).getAttributeValue();
-			attackDistanceSq = (int) (distance * distance);
+			attackDistanceSq = 20;
 		}
 		return attackDistanceSq;
 	}
@@ -33,7 +31,7 @@ public class NpcAIFactionRangedAttack extends NpcAIAttack<NpcBase> {
 	@Override
 	protected void doAttack(double dist) {
 		double homeDist = npc.getDistanceSqFromHome();
-		if (homeDist > MIN_RANGE && dist < 8 * 8) {
+		if (homeDist > MIN_RANGE && dist < 16) {
 			npc.addAITask(TASK_MOVE);
 			this.moveToPosition(npc.getHomePosition(), homeDist);
 		} else {
@@ -44,7 +42,7 @@ public class NpcAIFactionRangedAttack extends NpcAIAttack<NpcBase> {
 			float pwr = (float) (getAttackDistanceSq() / dist);
 			pwr = Math.min(Math.max(pwr, 0.1F), 1F);
 			this.rangedAttacker.attackEntityWithRangedAttack(getTarget(), pwr);
-			this.setAttackDelay(35);
+			this.setAttackDelay(80);
 		}
 	}
 }
--- src/main/java/net/shadowmage/ancientwarfare/npc/entity/faction/NpcFactionPriest.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/entity/faction/NpcFactionPriest.java
@@ -1,19 +1,37 @@
 package net.shadowmage.ancientwarfare.npc.entity.faction;
 
+import com.google.common.base.Predicate;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.IRangedAttackMob;
 import net.minecraft.entity.ai.EntityAIRestrictOpenDoor;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.ai.EntityAIWatchClosest2;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
+import net.minecraft.init.PotionTypes;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.potion.PotionType;
+import net.minecraft.potion.PotionUtils;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+import net.shadowmage.ancientwarfare.npc.ai.NpcAIAttackNearest;
 import net.shadowmage.ancientwarfare.npc.ai.NpcAIDoor;
 import net.shadowmage.ancientwarfare.npc.ai.NpcAIFollowPlayer;
+import net.shadowmage.ancientwarfare.npc.ai.NpcAIHurt;
 import net.shadowmage.ancientwarfare.npc.ai.NpcAIMoveHome;
 import net.shadowmage.ancientwarfare.npc.ai.NpcAIWander;
+import net.shadowmage.ancientwarfare.npc.ai.faction.NpcAIFactionPotionAttack;
 import net.shadowmage.ancientwarfare.npc.ai.faction.NpcAIFactionPriest;
+import net.shadowmage.ancientwarfare.npc.entity.EntityFactionPotion;
 
-public class NpcFactionPriest extends NpcFaction {
+public class NpcFactionPriest extends NpcFaction implements IRangedAttackMob {
 	public NpcFactionPriest(World world) {
 		super(world);
 		addAI();
@@ -25,20 +43,84 @@ public class NpcFactionPriest extends NpcFaction {
 	}
 
 	private void addAI() {
+		//noinspection Guava
+		Predicate<Entity> selector = entity -> {
+			//noinspection ConstantConditions
+			if (!isHostileTowards(entity)) {
+				return false;
+			}
+			if (hasHome()) {
+				BlockPos home = getHomePosition();
+				double dist = entity.getDistanceSq(home.getX() + 0.5d, home.getY(), home.getZ() + 0.5d);
+				if (dist > 30 * 30) {
+					return false;
+				}
+			}
+			return true;
+		};
+
 		this.tasks.addTask(0, new EntityAISwimming(this));
 		this.tasks.addTask(0, new EntityAIRestrictOpenDoor(this));
 		this.tasks.addTask(0, new NpcAIDoor(this, true));
 		this.tasks.addTask(1, new NpcAIFollowPlayer(this));
 		this.tasks.addTask(2, new NpcAIMoveHome(this, 50F, 5F, 30F, 5F));
 		this.tasks.addTask(3, new NpcAIFactionPriest(this));
+		this.tasks.addTask(16, new NpcAIFactionPotionAttack(this));
 
 		this.tasks.addTask(101, new EntityAIWatchClosest2(this, EntityPlayer.class, 3.0F, 1.0F));
 		this.tasks.addTask(102, new NpcAIWander(this));
 		this.tasks.addTask(103, new EntityAIWatchClosest(this, EntityLiving.class, 8.0F));
+
+		targetTasks.addTask(1, new NpcAIHurt(this));
+		targetTasks.addTask(15, new NpcAIAttackNearest(this, selector));
+	}
+
+	@Override
+	public void attackEntityWithRangedAttack(EntityLivingBase target, float distanceFactor) {
+		double d0 = target.posY + (double) target.getEyeHeight() - 1.100000023841858D;
+		double d1 = target.posX + target.motionX - this.posX;
+		double d2 = d0 - this.posY;
+		double d3 = target.posZ + target.motionZ - this.posZ;
+		float f = MathHelper.sqrt(d1 * d1 + d3 * d3);
+		PotionType potiontype = PotionTypes.HARMING;
+
+		if (f >= 8.0F && !target.isPotionActive(MobEffects.SLOWNESS)) {
+			potiontype = PotionTypes.SLOWNESS;
+		} else if (target.getHealth() >= 8.0F && !target.isPotionActive(MobEffects.POISON)) {
+			potiontype = PotionTypes.POISON;
+		} else if (f <= 3.0F && !target.isPotionActive(MobEffects.WEAKNESS) && this.rand.nextFloat() < 0.25F) {
+			potiontype = PotionTypes.WEAKNESS;
+		}
+
+		EntityFactionPotion entitypotion = new EntityFactionPotion(this.world, this, PotionUtils.addPotionToItemStack(new ItemStack(Items.SPLASH_POTION), potiontype), factionName);
+		entitypotion.rotationPitch -= 20.0F;
+		entitypotion.shoot(d1, d2 + (double) (f * 0.2F), d3, 0.75F, 8.0F);
+		this.world.playSound((EntityPlayer) null, this.posX, this.posY, this.posZ, SoundEvents.ENTITY_WITCH_THROW, this.getSoundCategory(), 1.0F, 0.8F + this.rand.nextFloat() * 0.4F);
+		this.world.spawnEntity(entitypotion);
+	}
+
+	@Override
+	public boolean canAttackClass(Class claz) {
+		return true;
+	}
+
+	@Override
+	public boolean isPassive() {
+		return false;
 	}
 
 	@Override
 	public String getNpcType() {
 		return "priest";
+	}
+
+	@Override
+	public boolean isPotionApplicable(PotionEffect potioneffectIn) {
+		if (potioneffectIn.getPotion() == MobEffects.POISON) {
+			net.minecraftforge.event.entity.living.PotionEvent.PotionApplicableEvent event = new net.minecraftforge.event.entity.living.PotionEvent.PotionApplicableEvent(this, potioneffectIn);
+			net.minecraftforge.common.MinecraftForge.EVENT_BUS.post(event);
+			return event.getResult() == net.minecraftforge.fml.common.eventhandler.Event.Result.ALLOW;
+		}
+		return super.isPotionApplicable(potioneffectIn);
 	}
 }
