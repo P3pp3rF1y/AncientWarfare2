70dce0df677ffa15d40918b1045e5e0946d9f269
WinDanesz <win.danesz@users.noreply.github.com>
2020-01-19 15:52:47 +0100

    Added Gate/Drawbridge moving, hurt, break sound, Changed Gate/Drawbridge resistance against tools and weapons

--- src/main/java/net/shadowmage/ancientwarfare/structure/gates/types/GateDouble.java
+++ src/main/java/net/shadowmage/ancientwarfare/structure/gates/types/GateDouble.java
@@ -1,5 +1,6 @@
 package net.shadowmage.ancientwarfare.structure.gates.types;
 
+import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.shadowmage.ancientwarfare.core.util.BlockTools;
@@ -10,8 +11,8 @@ public class GateDouble extends Gate {
 	 * @param id
 	 * @param textureLocation
 	 */
-	public GateDouble(int id, String textureLocation) {
-		super(id, textureLocation);
+	public GateDouble(int id, String textureLocation, SoundEvent moveSound, SoundEvent hurtSound, SoundEvent breakSound) {
+		super(id, textureLocation, moveSound, hurtSound, breakSound);
 	}
 
 	@Override
