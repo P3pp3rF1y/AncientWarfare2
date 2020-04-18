9e3af3e2d3a4c9f880eaebf2d005ba8a816760f8
P3pp3rF1y <p3pp3rf1y@gmail.com>
2020-04-18 12:29:54 +0200

    Fixes chairs not to rotate when a player sits on them.

--- src/main/java/net/shadowmage/ancientwarfare/structure/tile/TileChair.java
+++ src/main/java/net/shadowmage/ancientwarfare/structure/tile/TileChair.java
@@ -5,7 +5,10 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.shadowmage.ancientwarfare.core.block.BlockRotationHandler;
+import net.shadowmage.ancientwarfare.core.render.property.CoreProperties;
+import net.shadowmage.ancientwarfare.structure.util.BlockStateProperties;
 
 import java.util.Set;
 
@@ -58,5 +61,11 @@ public class TileChair extends TileMulti implements BlockRotationHandler.IRotata
 	@Override
 	public void setPrimaryFacing(EnumFacing face) {
 		facing = face;
+	}
+
+	@Override
+	public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newState) {
+		return oldState.getValue(BlockStateProperties.VARIANT) != newState.getValue(BlockStateProperties.VARIANT)
+				|| !oldState.getValue(CoreProperties.VISIBLE).equals(newState.getValue(CoreProperties.VISIBLE));
 	}
 }
