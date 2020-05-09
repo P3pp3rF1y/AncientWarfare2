a01e1a3a4bf0697eea4be0c674ec44a11ee7d1d0
P3pp3rF1y <p3pp3rf1y@gmail.com>
2020-05-09 16:23:48 +0200

    Added 3rd mode to info tool to generate loot entry json for use in loot tables.

--- src/main/java/net/shadowmage/ancientwarfare/core/item/ItemInfoTool.java
+++ src/main/java/net/shadowmage/ancientwarfare/core/item/ItemInfoTool.java
@@ -1,5 +1,8 @@
 package net.shadowmage.ancientwarfare.core.item;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
@@ -12,6 +15,17 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
+import net.minecraft.world.storage.loot.LootEntry;
+import net.minecraft.world.storage.loot.LootEntryItem;
+import net.minecraft.world.storage.loot.RandomValueRange;
+import net.minecraft.world.storage.loot.conditions.LootCondition;
+import net.minecraft.world.storage.loot.conditions.LootConditionManager;
+import net.minecraft.world.storage.loot.functions.LootFunction;
+import net.minecraft.world.storage.loot.functions.LootFunctionManager;
+import net.minecraft.world.storage.loot.functions.SetCount;
+import net.minecraft.world.storage.loot.functions.SetDamage;
+import net.minecraft.world.storage.loot.functions.SetMetadata;
+import net.minecraft.world.storage.loot.functions.SetNBT;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.shadowmage.ancientwarfare.core.gui.GuiInfoTool;
@@ -22,10 +36,18 @@ import net.shadowmage.ancientwarfare.core.util.ItemTools;
 import javax.annotation.Nullable;
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class ItemInfoTool extends ItemBaseCore {
+	private static final Gson GSON = new GsonBuilder().setPrettyPrinting()
+			.registerTypeAdapter(RandomValueRange.class, new RandomValueRange.Serializer())
+			.registerTypeHierarchyAdapter(LootEntry.class, new LootEntry.Serializer())
+			.registerTypeHierarchyAdapter(LootFunction.class, new LootFunctionManager.Serializer())
+			.registerTypeHierarchyAdapter(LootCondition.class, new LootConditionManager.Serializer())
+			.create();
+
 	public ItemInfoTool() {
 		super("info_tool");
 		setMaxStackSize(1);
@@ -33,7 +55,7 @@ public class ItemInfoTool extends ItemBaseCore {
 
 	@Override
 	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
-		tooltip.add(getMode(stack).toString() + " mode");
+		tooltip.add(getMode(stack).getDisplayName() + " mode");
 	}
 
 	private void printSimpleMessage(EntityPlayer player, IBlockState state) {
@@ -49,17 +71,21 @@ public class ItemInfoTool extends ItemBaseCore {
 
 	private void printJSON(EntityPlayer player, IBlockState state) {
 		String json = BlockTools.serializeToJson(state).toString();
-		StringSelection stringSelection = new StringSelection(json);
-		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
-		player.sendMessage(new TextComponentString(json));
-		player.sendMessage(new TextComponentString("Copied to clipboard"));
+		printAndCopyToClipboard(player, json);
 	}
 
 	public void printItemInfo(EntityPlayer player, ItemStack infoTool, ItemStack stack) {
-		if (getMode(infoTool) == Mode.INFO) {
-			printSimpleMessage(player, stack);
-		} else {
-			printJSON(player, stack);
+		Mode mode = getMode(infoTool);
+		switch (mode) {
+			case INFO:
+				printSimpleMessage(player, stack);
+				break;
+			case JSON:
+				printJSON(player, stack);
+				break;
+			case LOOT_ENTRY:
+				printLootEntryJSON(player, stack);
+				break;
 		}
 	}
 
@@ -75,6 +101,10 @@ public class ItemInfoTool extends ItemBaseCore {
 
 	private void printJSON(EntityPlayer player, ItemStack stack) {
 		String json = ItemTools.serializeToJson(stack).toString();
+		printAndCopyToClipboard(player, json);
+	}
+
+	private void printAndCopyToClipboard(EntityPlayer player, String json) {
 		StringSelection stringSelection = new StringSelection(json);
 		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
 		player.sendMessage(new TextComponentString(json));
@@ -88,12 +118,20 @@ public class ItemInfoTool extends ItemBaseCore {
 		}
 
 		RayTraceResult hit = rayTrace(world, player, true);
+		//noinspection ConstantConditions
 		if (hit != null && hit.typeOfHit == RayTraceResult.Type.BLOCK) {
 			IBlockState state = world.getBlockState(hit.getBlockPos());
-			if (getMode(player.getHeldItem(hand)) == Mode.INFO) {
-				printSimpleMessage(player, state);
-			} else {
-				printJSON(player, state);
+			Mode mode = getMode(player.getHeldItem(hand));
+			switch (mode) {
+				case INFO:
+					printSimpleMessage(player, state);
+					break;
+				case JSON:
+					printJSON(player, state);
+					break;
+				case LOOT_ENTRY:
+					printLootEntryJSON(player, state, hit, world);
+					break;
 			}
 			return new ActionResult<>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
 		}
@@ -106,19 +144,40 @@ public class ItemInfoTool extends ItemBaseCore {
 		}
 	}
 
+	private void printLootEntryJSON(EntityPlayer player, IBlockState state, RayTraceResult hit, World world) {
+		printLootEntryJSON(player, state.getBlock().getPickBlock(state, hit, world, hit.getBlockPos(), player));
+	}
+
+	private void printLootEntryJSON(EntityPlayer player, ItemStack stack) {
+		List<LootFunction> functions = new ArrayList<>();
+		functions.add(new SetCount(new LootCondition[0], new RandomValueRange(1)));
+		if (stack.hasTagCompound()) {
+			//noinspection ConstantConditions
+			functions.add(new SetNBT(new LootCondition[0], stack.getTagCompound()));
+		}
+		if (stack.isItemStackDamageable()) {
+			functions.add(new SetDamage(new LootCondition[0], new RandomValueRange(stack.getItemDamage())));
+		} else if (stack.getMetadata() > 0) {
+			functions.add(new SetMetadata(new LootCondition[0], new RandomValueRange(stack.getMetadata())));
+		}
+		//noinspection ConstantConditions
+		LootEntry lootEntry = new LootEntryItem(stack.getItem(), 1, 0, functions.toArray(new LootFunction[0]), new LootCondition[0], stack.getItem().getRegistryName().toString());
+		printAndCopyToClipboard(player, GSON.toJson(lootEntry));
+	}
+
 	private ItemStack cycleMode(ItemStack stack) {
 		if (!stack.hasTagCompound()) {
 			stack.setTagCompound(new NBTTagCompound());
 		}
 		//noinspection ConstantConditions
-		stack.getTagCompound().setString("mode", getMode(stack) == Mode.INFO ? "json" : "info");
+		stack.getTagCompound().setString("mode", getMode(stack).cycle().name().toLowerCase());
 		return stack;
 	}
 
 	private Mode getMode(ItemStack stack) {
 		if (stack.hasTagCompound()) {
 			//noinspection ConstantConditions
-			return stack.getTagCompound().getString("mode").equals("json") ? Mode.JSON : Mode.INFO;
+			return Mode.fromString(stack.getTagCompound().getString("mode"));
 		}
 		return Mode.INFO;
 	}
@@ -132,7 +191,44 @@ public class ItemInfoTool extends ItemBaseCore {
 	}
 
 	enum Mode {
-		INFO,
-		JSON
+		INFO("Info"),
+		JSON("JSON"),
+		LOOT_ENTRY("Loot Entry");
+
+		private String displayName;
+
+		Mode(String displayName) {
+			this.displayName = displayName;
+		}
+
+		private static final Map<String, Mode> NAME_MODE_MAP;
+
+		static {
+			ImmutableMap.Builder<String, Mode> builder = new ImmutableMap.Builder<>();
+			for (Mode mode : Mode.values()) {
+				builder.put(mode.name().toLowerCase(), mode);
+			}
+			NAME_MODE_MAP = builder.build();
+		}
+
+		public Mode cycle() {
+			switch (this) {
+				case INFO:
+					return JSON;
+				case JSON:
+					return LOOT_ENTRY;
+				case LOOT_ENTRY:
+					return INFO;
+			}
+			return INFO;
+		}
+
+		public static Mode fromString(String mode) {
+			return NAME_MODE_MAP.getOrDefault(mode.toLowerCase(), INFO);
+		}
+
+		public String getDisplayName() {
+			return displayName;
+		}
 	}
 }
