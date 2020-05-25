a39caa3074b0fb1ed35c5b793925116a8089e148
WinDanesz <31292708+WinDanesz@users.noreply.github.com>
2020-05-26 00:03:36 +0200

    No friendly fire between faction spellcasters and their allies

---
+++ src/main/java/net/shadowmage/ancientwarfare/npc/compat/ebwizardry/FactionAllyDesignation.java
@@ -0,0 +1,25 @@
+package net.shadowmage.ancientwarfare.npc.compat.ebwizardry;
+
+import electroblob.wizardry.util.IElementalDamage;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.shadowmage.ancientwarfare.npc.entity.faction.NpcFaction;
+
+public final class FactionAllyDesignation {
+
+	// Prevents any magic damage to friendly NPCs
+	@SubscribeEvent
+	public static void onLivingAttackEvent(LivingAttackEvent event) {
+		if (event.getSource() != null && event.getSource().getTrueSource() instanceof NpcFaction
+				&& event.getSource() instanceof IElementalDamage) {
+
+			if (event.getEntity() instanceof NpcFaction) {
+				NpcFaction caster = (NpcFaction) event.getSource().getTrueSource();
+				NpcFaction target = (NpcFaction) event.getEntity();
+				if (target.getFaction().equals(caster.getFaction())) {
+					event.setCanceled(true);
+				}
+			}
+		}
+	}
+}
