c689dc32260a585611103c32ec4c1c777479e465
WinDanesz <31292708+WinDanesz@users.noreply.github.com>
2020-05-26 14:59:42 +0200

    Fixed ItemBlockInfo sending messages only to console

--- src/main/java/net/shadowmage/ancientwarfare/structure/item/ItemBlockInfo.java
+++ src/main/java/net/shadowmage/ancientwarfare/structure/item/ItemBlockInfo.java
@@ -2,16 +2,20 @@ package net.shadowmage.ancientwarfare.structure.item;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.shadowmage.ancientwarfare.core.util.BlockTools;
 import net.shadowmage.ancientwarfare.core.util.WorldTools;
-import net.shadowmage.ancientwarfare.structure.AncientWarfareStructure;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ItemBlockInfo extends ItemBaseStructure {
 
@@ -27,12 +31,17 @@ public class ItemBlockInfo extends ItemBaseStructure {
 				IBlockState state = world.getBlockState(pos);
 				Block block = state.getBlock();
 				//noinspection ConstantConditions
-				AncientWarfareStructure.LOG.info("block: " + block.getRegistryName().toString() + ", meta: " + block.getMetaFromState(state));
+				player.sendMessage(new TextComponentString("block: " + block.getRegistryName().toString() + ", meta: " + block.getMetaFromState(state)));
 				if (block.hasTileEntity(state)) {
-					AncientWarfareStructure.LOG.info("tile: " + WorldTools.getTile(world, pos).map(t -> t.getClass().toString()).orElse(""));
+					player.sendMessage(new TextComponentString("tile: " + WorldTools.getTile(world, pos).map(t -> t.getClass().toString()).orElse("")));
 				}
 			}
 		}
 		return new ActionResult<>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
+	}
+
+	@Override
+	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
+		tooltip.add("Right click a Block to print block info");
 	}
 }
