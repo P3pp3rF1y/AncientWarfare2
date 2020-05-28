f472aea5b3752dd7a5c967dbaa4ba1eaeb435f28
P3pp3rF1y <p3pp3rf1y@gmail.com>
2020-05-28 21:42:15 +0200

    Dupe bug fix

--- src/main/java/net/shadowmage/ancientwarfare/npc/inventory/NpcEquipmentHandler.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/inventory/NpcEquipmentHandler.java
@@ -2,50 +2,136 @@ package net.shadowmage.ancientwarfare.npc.inventory;
 
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.NonNullList;
-import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.ItemHandlerHelper;
 import net.shadowmage.ancientwarfare.npc.entity.NpcBase;
 import net.shadowmage.ancientwarfare.npc.item.ItemUpkeepOrder;
 
 import javax.annotation.Nonnull;
 
-public class NpcEquipmentHandler extends ItemStackHandler {
+public class NpcEquipmentHandler implements IItemHandlerModifiable {
 
 	private static final int SIZE_INVENTORY = 8;
 	private final NpcBase npc;
 
 	public NpcEquipmentHandler(NpcBase npc) {
-		super(SIZE_INVENTORY);
 		this.npc = npc;
-		stacks = NonNullList.withSize(SIZE_INVENTORY, ItemStack.EMPTY);
-		for (int i = 0; i < SIZE_INVENTORY; i++) {
-			if (!npc.getItemStackFromSlot(i).isEmpty()) {
-				stacks.set(i, npc.getItemStackFromSlot(i).copy());
-			}
-		}
+	}
+
+	@Override
+	public int getSlots() {
+		return SIZE_INVENTORY;
+	}
+
+	private int validateSlotIndex(final int slot) {
+		if (slot < 0 || slot >= SIZE_INVENTORY) { throw new IllegalArgumentException("Slot " + slot + " not in valid range - [0," + SIZE_INVENTORY + ")"); }
+		return slot;
 	}
 
 	@Nonnull
 	@Override
-	public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
-		return isItemStackValidForSlot(slot, stack) ? super.insertItem(slot, stack, simulate) : stack;
+	public ItemStack getStackInSlot(int slot) {
+		return npc.getItemStackFromSlot(validateSlotIndex(slot));
 	}
 
-	private boolean isItemStackValidForSlot(int slot, @Nonnull ItemStack stack) {
-		if (stack.isEmpty() || slot < 0) {
-			return false;
+	@Nonnull
+	@Override
+	public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
+		if (stack.isEmpty()) {
+			return ItemStack.EMPTY;
 		}
-		if (slot == NpcBase.UPKEEP_SLOT)
-			return stack.getItem() instanceof ItemUpkeepOrder;
-		else if (slot == NpcBase.ORDER_SLOT)
-			return npc.isValidOrdersStack(stack);
-		else if (slot > 1 && slot < NpcBase.ORDER_SLOT)//armors
-			return stack.getItem().isValidArmor(stack, EntityEquipmentSlot.values()[slot], npc);
-		return true;//weapon/tool, shield slot   TODO add slot validation ?
+
+		validateSlotIndex(slot);
+
+		ItemStack existing = npc.getItemStackFromSlot(slot);
+
+		int limit = getStackLimit(slot, stack);
+
+		if (!existing.isEmpty()) {
+			if (!ItemHandlerHelper.canItemStacksStack(stack, existing)) {
+				return stack;
+			}
+
+			limit -= existing.getCount();
+		}
+
+		if (limit <= 0) {
+			return stack;
+		}
+
+		boolean reachedLimit = stack.getCount() > limit;
+
+		if (!simulate) {
+			if (existing.isEmpty()) {
+				npc.setItemStackToSlot(slot, reachedLimit ? ItemHandlerHelper.copyStackWithSize(stack, limit) : stack);
+			} else {
+				existing.grow(reachedLimit ? limit : stack.getCount());
+			}
+		}
+
+		return reachedLimit ? ItemHandlerHelper.copyStackWithSize(stack, stack.getCount() - limit) : ItemStack.EMPTY;
+
+	}
+
+	private int getStackLimit(final int slot, ItemStack stack) {
+		return Math.min(getSlotLimit(slot), stack.getMaxStackSize());
+	}
+
+	@Nonnull
+	@Override
+	public ItemStack extractItem(int slot, int amount, boolean simulate) {
+		if (amount == 0) {
+			return ItemStack.EMPTY;
+		}
+
+		validateSlotIndex(slot);
+
+		ItemStack existing = npc.getItemStackFromSlot(slot);
+
+		if (existing.isEmpty()) {
+			return ItemStack.EMPTY;
+		}
+
+		final int toExtract = Math.min(amount, existing.getMaxStackSize());
+
+		if (existing.getCount() <= toExtract) {
+			if (!simulate) {
+				npc.setItemStackToSlot(slot, ItemStack.EMPTY);
+			}
+
+			return existing;
+		} else {
+			if (!simulate) {
+				npc.setItemStackToSlot(slot, ItemHandlerHelper.copyStackWithSize(existing, existing.getCount() - toExtract));
+			}
+
+			return ItemHandlerHelper.copyStackWithSize(existing, toExtract);
+		}
 	}
 
 	@Override
-	protected void onContentsChanged(int slot) {
-		npc.setItemStackToSlot(slot, stacks.get(slot));
+	public int getSlotLimit(int slot) {
+		return slot == NpcBase.UPKEEP_SLOT || slot == NpcBase.ORDER_SLOT || slot > 1 && slot < NpcBase.ORDER_SLOT ? 1 : 64;
+	}
+
+	@Override
+	public boolean isItemValid(int slot, ItemStack stack) {
+		if (slot == NpcBase.UPKEEP_SLOT) {
+			return stack.getItem() instanceof ItemUpkeepOrder;
+		} else if (slot == NpcBase.ORDER_SLOT) {
+			return npc.isValidOrdersStack(stack);
+		} else if (slot > 1 && slot < NpcBase.ORDER_SLOT) {
+			return stack.getItem().isValidArmor(stack, EntityEquipmentSlot.values()[slot], npc);
+		}
+		return true;
+	}
+
+	@Override
+	public void setStackInSlot(int slot, ItemStack stack) {
+		validateSlotIndex(slot);
+		if (ItemStack.areItemStacksEqual(npc.getItemStackFromSlot(slot), stack)) {
+			return;
+		}
+		npc.setItemStackToSlot(slot, stack);
 	}
 }
