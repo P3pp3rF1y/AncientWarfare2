d544eaeaaeb31e77d3e0797b7b7bfaabb8387c98
WinDanesz <31292708+WinDanesz@users.noreply.github.com>
2021-01-18 01:23:15 +0100

    removed /awfaction set and /awfaction setall commands

--- src/main/java/net/shadowmage/ancientwarfare/npc/command/CommandFaction.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/command/CommandFaction.java
@@ -16,7 +16,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class CommandFaction extends CommandBase {
-	private static final String COMMAND_AW_FACTION_SET_USAGE_UNLOC = "command.aw.faction.set.usage";
 
 	@Override
 	public int getRequiredPermissionLevel() {
@@ -40,11 +39,7 @@ public class CommandFaction extends CommandBase {
 		}
 		String cmd = var2[0];
 		String playerName = var2[1];
-		if (cmd.equalsIgnoreCase("set")) {
-			setFactionStanding(var1, var2, playerName);
-		} else if (cmd.equalsIgnoreCase("setall")) {
-			setAllFactionStandings(var1, var2, playerName);
-		} else if (cmd.equalsIgnoreCase("get")) {
+		if (cmd.equalsIgnoreCase("get")) {
 			showFactionStandings(var1, playerName);
 		}
 	}
@@ -58,52 +53,12 @@ public class CommandFaction extends CommandBase {
 		}
 	}
 
-	private void setAllFactionStandings(ICommandSender var1, String[] var2, String playerName) throws WrongUsageException {
-		if (var2.length < 3) {
-			throw new WrongUsageException("command.aw.faction.setall.usage");
-		}
-		String amount = var2[2];
-		int amt;
-		try {
-			amt = Integer.parseInt(amount);
-		}
-		catch (NumberFormatException e) {
-			throw new WrongUsageException("command.aw.faction.setall.usage");
-		}
-		for (String faction : FactionRegistry.getFactionNames()) {
-			FactionTracker.INSTANCE.setStandingFor(var1.getEntityWorld(), playerName, faction, amt);
-			var1.sendMessage(new TextComponentTranslation("command.aw.faction.set", playerName, faction, amt));
-		}
-	}
-
-	private void setFactionStanding(ICommandSender var1, String[] var2, String playerName) throws WrongUsageException {
-		if (var2.length < 4) {
-			throw new WrongUsageException(COMMAND_AW_FACTION_SET_USAGE_UNLOC);
-		}
-		String faction = var2[2];
-		if (FactionRegistry.getFactionNames().stream().noneMatch(f -> f.equalsIgnoreCase(faction))) {
-			throw new WrongUsageException(COMMAND_AW_FACTION_SET_USAGE_UNLOC);
-		}
-		String amount = var2[3];
-		int amt;
-		try {
-			amt = Integer.parseInt(amount);
-		}
-		catch (NumberFormatException e) {
-			throw new WrongUsageException(COMMAND_AW_FACTION_SET_USAGE_UNLOC);
-		}
-		FactionTracker.INSTANCE.setStandingFor(var1.getEntityWorld(), playerName, faction, amt);
-		var1.sendMessage(new TextComponentTranslation("command.aw.faction.set", playerName, faction, amt));
-	}
-
 	@Override
 	public List<String> getTabCompletions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos targetPos) {
 		if (args.length == 1) {
-			return CommandBase.getListOfStringsMatchingLastWord(args, "set", "setall", "get");
+			return CommandBase.getListOfStringsMatchingLastWord(args, "get");
 		} else if (args.length == 2) {
 			return CommandBase.getListOfStringsMatchingLastWord(args, server.getOnlinePlayerNames());
-		} else if (args.length == 3 && args[0].equalsIgnoreCase("set")) {
-			return CommandBase.getListOfStringsMatchingLastWord(args, FactionRegistry.getFactionNames());
 		}
 		return Collections.emptyList();
 	}
