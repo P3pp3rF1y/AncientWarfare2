e9b9eeca6424eb87e84cc0c2876e759ae115cbe6
WinDanesz <31292708+WinDanesz@users.noreply.github.com>
2021-05-28 22:19:25 +0200

    Fixes player owned tamed mounts and pets being targeted by friendly Tribe or Nations npc's #1276

--- src/main/java/net/shadowmage/ancientwarfare/npc/ai/AIHelper.java
+++ src/main/java/net/shadowmage/ancientwarfare/npc/ai/AIHelper.java
@@ -3,12 +3,15 @@ package net.shadowmage.ancientwarfare.npc.ai;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.IEntityOwnable;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
 import net.shadowmage.ancientwarfare.npc.entity.NpcBase;
 
 import javax.annotation.Nullable;
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 public final class AIHelper {
@@ -38,4 +41,14 @@ public final class AIHelper {
 	public static boolean isAdditionalEntityToTarget(Entity e) {
 		return additionalHostileEntitiesToTarget.contains(e.getEntityId());
 	}
+
+	public static Optional<EntityPlayer> getOwnerPlayer(IEntityOwnable entity, World world) {
+		if (entity.getOwner() != null) {
+			EntityPlayer player = world.getPlayerEntityByUUID(entity.getOwner().getUniqueID());
+
+			return player != null ? Optional.of(player) : Optional.empty();
+		}
+		return Optional.empty();
+	}
+
 }
